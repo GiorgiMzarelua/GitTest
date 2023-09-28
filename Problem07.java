6cf32e19a51204a19ea2857c1610515c58231f54
S <s>
2023-09-29 00:01:28 +0400

    {"types":"C","runs":17}

--- Problem07.java
+++ Problem07.java
@@ -1,4 +1,31 @@
-
-public class Problem07 {
-
+import stanford.karel.Karel;
+//კარელი დგას 1x1 უჯრაზე, შეავსებინეთ მას მთელი სამყარო თითო ბრილიანტით. ანუ მთელ
+//სამყაროში ყველა უჯრაზე უნდა იდოს ზუსტად ერთი ბრილიანტი. ჩათვალეთ რომ საწყისი
+//სამყარო ცარიელი არ არის და გარკვეულ(ჩვენთვის უცნობ) უჯრებში თითო(მხოლოდ ერთი)
+//ბრილიანტი დევს. ამასთან გაითვალისწინეთ, რომ სამყაროს ზომები თქვენთვის უცნობია და
+//თქვენი პროგრამა უნდა მუშაობდეს ნებისმიერი ზომის სამყაროსათვის.
+public class Problem07 extends Karel{
+	public void run(){
+		int length=0;
+		int width=0;
+		while(frontIsClear()){
+			if(noBeepersPresent()){
+				putBeeper();
+			}
+			move();
+			width++;
+		}
+		turnLeft();
+		while(frontIsClear()){
+			if(noBeepersPresent()){
+				putBeeper();
+			}
+			move();
+			length++;
+		}
+        while(width>0){
+        	facingEast();
+        	
+        }
+	}
 }
