1f14b4ba2ff91996751c9d82c03ec80bc1667ec0
S <s>
2023-09-29 22:05:33 +0400

    {"types":"C","runs":1}

--- test.java
+++ test.java
@@ -1,4 +1,47 @@
-
-public class test {
-
+import stanford.karel.Karel;
+public class test extends Karel{
+    public void run(){
+    	facingEast();
+    	int width=0;
+		int length=0;
+		while(frontIsClear()){
+			if(noBeepersPresent()){
+				putBeeper();
+			}
+			move();
+			width++;
+		}
+		turnLeft();
+		while(frontIsClear()){
+			if(noBeepersPresent()){
+				putBeeper();
+			}
+			move();
+			length++;
+		}
+		for(int i=1;i<=width;i++){
+			int a=length-1;
+			if(i%2==1){
+				turnLeft();
+				move();
+				turnLeft();
+			}
+			if(i%2==0){
+				turnLeft();
+				turnLeft();
+				turnLeft();
+				move();
+				turnLeft();
+				turnLeft();
+				turnLeft();
+			}
+			while(a>0){
+				move();
+				if(noBeepersPresent()){
+					putBeeper();
+				}
+				a--;
+			}
+		}
+    }
 }
