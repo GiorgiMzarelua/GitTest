eb05539e298ead8cf5b19b7f243e7f559d42d005
S <s>
2023-10-03 17:48:05 +0400

    {"types":"C","runs":16}

--- test.java
+++ test.java
@@ -1,30 +1,8 @@
 import stanford.karel.Karel;
 public class test extends Karel{
     public void run(){
-    	move();
-		move();
-		turnLeft();
-		putBeeper();
-		for(int i=0;i<3;i++){
+    	for(int i=0;i<7;i++){
 			move();
-			putBeeper();
 		}
-		turnLeft();
-		turnLeft();
-		turnLeft();
-		move();
-		putBeeper();
-		move();
-		putBeeper();
-		turnLeft();
-		turnLeft();
-		turnLeft();
-		for(int i=0;i<3;i++)
-		{
-			move();
-			putBeeper();
-		}
-		turnLeft();
-		turnLeft();
     }
 }
