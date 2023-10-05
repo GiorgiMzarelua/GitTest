15bf7f698f0bc8e000d3ed15fedbabff80089ea0
S <s>
2023-10-05 13:18:10 +0400

    {"types":"C","runs":1}

--- Problem07.java
+++ Problem07.java
@@ -6,47 +6,28 @@ import stanford.karel.Karel;
 //თქვენი პროგრამა უნდა მუშაობდეს ნებისმიერი ზომის სამყაროსათვის.
 public class Problem07 extends Karel{
     public void run(){
-    	facingEast();
-    	int width=0;
-		int length=0;
-		while(frontIsClear()){
-			if(noBeepersPresent()){
-				putBeeper();
-			}
-			move();
-			width++;
-		}
-		turnLeft();
-		while(frontIsClear()){
-			if(noBeepersPresent()){
-				putBeeper(); 
-			}
-			move();
-			length++;
-		}
-		int a=length-1;
-		for(int i=0;i<width;i++){
-			if(i%2==0){
-				turnLeft();
-				move();
-				turnLeft();
-			}
-			if(i%2==1){
-				turnLeft();
-				turnLeft();
-				turnLeft();
-				move();
-				turnLeft();
-				turnLeft();
-				turnLeft();
-			}
-			while(a>0){
-				move();
-				if(noBeepersPresent()){
-					putBeeper();
-				}
-				a--;
-			}
-		}
+    	while(leftIsClear()){
+    		while(frontIsClear()){
+    			if(noBeepersPresent()){
+    				putBeeper();
+    			}
+    			move();
+    		}
+    		if(noBeepersPresent()){
+    			putBeeper();
+    		}
+    		turnLeft();
+    		turnLeft();
+    		while(frontIsClear()){
+    			move();
+    		}
+    		turnLeft();
+    		turnLeft();
+    		turnLeft();
+    		move();
+    		turnLeft();
+    		turnLeft();
+    		turnLeft();
+    	}
     }
 }
