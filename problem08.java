91e3994a18ca422a0c5dc6d673c8128bb3e780f0
S <s>
2023-10-05 09:50:54 +0400

    {"types":"C","runs":1}

--- problem08.java
+++ problem08.java
@@ -1,4 +1,20 @@
-
-public class problem08 {
-
+import stanford.karel.Karel;
+//კარელმა უნდა გამოაკლოს ორი რიცხვი, 2x1 უჯრაზე დევს n ცალი ბრილიანტი, 3x1 უჯრაზე
+//m ცალი ბრილიანტი, კარელმა 4x1 უჯრაზე უნდა დადოს n-m ბრილიანტი. ჩათვალეთ, რომ
+//კარელს ჩანთაში უსასრულო რაოდენობის ბრილიანტი აქვს და ჩათვალეთ, რომ n>=m.
+public class problem08 extends Karel{
+    public void run(){
+    	move();
+    	move();
+    	while(beepersPresent()){
+    		pickBeeper();
+    		turnLeft();
+    		turnLeft();
+    		move();
+    		pickBeeper();
+    		turnLeft();
+    		turnLeft();
+    		move();
+    	}
+    }
 }
