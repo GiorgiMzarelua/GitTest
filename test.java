c286939458082e37e23bcc8057353525c110e3ee
S <s>
2023-10-10 11:26:57 +0400

    {"types":"C","runs":14}

--- test.java
+++ test.java
@@ -1,46 +1,16 @@
 import stanford.karel.SuperKarel;
 public class test extends SuperKarel{
     public void run(){
-    	putBeeper();
-    	while(leftIsClear()){
-    		fillInLine();
-    		ascend();
-    	}
-    	fillInLine();
-    }
-
-	private void ascend() {
-		if(leftIsClear() & beepersPresent()){
-			turnLeft();
-			move();
-			turnRight();
-			move();
-			putBeeper();
-		}
-		if(leftIsClear() & noBeepersPresent()){
-			turnLeft();
-			move();
-			turnRight();
-			putBeeper();
-		}
+        turnLeft();
+        while(frontIsClear()){
+        	moveUp();
+        }
 	}
 
-	private void fillInLine() {
-		while(frontIsClear()){
-			move();
-			if(frontIsClear()){
-				move();
-				putBeeper();
-			}
-		}
-		turnBack();
-	}
-
-	private void turnBack() {
-		turnAround();
-		while(frontIsClear()){
-			move();
-		}
-		turnAround();
+	private void moveUp() {
+		move();
+		turnRight();
+		move();
+		turnLeft();
 	}
 }
