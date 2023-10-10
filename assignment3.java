6f5146c566d55799d0bee783a4337035db7dbc82
S <s>
2023-10-10 13:09:55 +0400

    {"types":"C","runs":4}

--- assignment3.java
+++ assignment3.java
@@ -1,4 +1,56 @@
+import stanford.karel.SuperKarel;
+public class assignment3 extends SuperKarel{
+    public void run(){
+    	putBeeper();
+    	while(leftIsClear()){
+    		fillInLine();
+    		ascend();
+    	}
+    	fillInLastLine();
+    }
 
-public class assignment3 {
+	private void fillInLastLine() {
+		while(frontIsClear()){
+			move();
+			if(frontIsClear()){
+				move();
+				putBeeper();
+			}
+		}
+	}
 
+	private void ascend() {
+		if(leftIsClear() & beepersPresent()){
+			turnLeft();
+			move();
+			turnRight();
+			move();
+			putBeeper();
+		}
+		if(leftIsClear() & noBeepersPresent()){
+			turnLeft();
+			move();
+			turnRight();
+			putBeeper();
+		}
+	}
+
+	private void fillInLine() {
+		while(frontIsClear()){
+			move();
+			if(frontIsClear()){
+				move();
+				putBeeper();
+			}
+		}
+		turnBack();
+	}
+
+	private void turnBack() {
+		turnAround();
+		while(frontIsClear()){
+			move();
+		}
+		turnAround();
+	}
 }
