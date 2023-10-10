f1fb241f24e2242515f2ca9f79b3754788829b2a
S <s>
2023-10-10 13:07:34 +0400

    {"types":"C","runs":2}

--- assignment1.java
+++ assignment1.java
@@ -1,5 +1,27 @@
+import stanford.karel.SuperKarel;
+public class assignment1 extends SuperKarel{
+    public void run(){
+    	findAmagazine();
+    	pickBeeper();
+    	backTostart();
+    }
 
+	private void backTostart() {
+		turnAround();
+		while(frontIsClear()){
+			move();
+		}
+		turnRight();
+		move();
+		turnRight();
+	}
 
-public class assignment1 {
-
+	private void findAmagazine() {
+		turnRight();
+		move();
+		turnLeft();
+		while(noBeepersPresent()){
+			move();
+		}
+	}
 }
