37abe5cf422410b29675e71e2925216370ab814a
S <s>
2023-10-12 17:31:12 +0400

    {"types":"C","runs":8}

--- problem12.java
+++ problem12.java
@@ -1,4 +1,18 @@
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
 
-public class problem12 {
+public class problem12 extends GraphicsProgram{
+	public void run(){
+		drawMidline();
+//		drawHouse();
+	}
 
+	private void drawMidline() {
+		double x1=0;
+		double x2=getWidth();
+		double y1=getHeight()/2;
+		double y2=y1;
+		GLine Line = new GLine(x1, y1, x2, y2);
+		add(Line);
+	}
 }
