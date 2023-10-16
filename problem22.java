0c4f5264a6038b7ed07a4b3b3e799ac937bdffae
S <s>
2023-10-16 23:40:13 +0400

    {"types":"C","runs":29}

--- problem22.java
+++ problem22.java
@@ -19,10 +19,29 @@ public class problem22 extends GraphicsProgram{
 			rec.setColor(Color.BLACK);
 			add(rec, x, y);
 		}
+		
 		for(int i = 0; i < 4; i++){
 			GRect rec = new GRect(getWidth() / 8, getHeight() / 8);
 			double x = (double) (getWidth() *  i / 4);
 			double y = (double) (getHeight() / 4);
+			rec.setFilled(true);
+			rec.setColor(Color.BLACK);
+			add(rec, x, y);
+		}
+		
+		for(int i = 0; i < 4; i++){
+			GRect rec = new GRect(getWidth() / 8, getHeight() / 8);
+			double x = (double) (getWidth() *  i / 4);
+			double y = (double) (getHeight() / 2);
+			rec.setFilled(true);
+			rec.setColor(Color.BLACK);
+			add(rec, x, y);
+		}
+		
+		for(int i = 0; i < 4; i++){
+			GRect rec = new GRect(getWidth() / 8, getHeight() / 8);
+			double x = (double) (getWidth() *  i / 4);
+			double y = (double) (getHeight() * 3 / 4);
 			rec.setFilled(true);
 			rec.setColor(Color.BLACK);
 			add(rec, x, y);
