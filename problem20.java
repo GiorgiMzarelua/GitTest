c13fd3c225dff937cae2fb7d9d9398cb9cba9b33
S <s>
2023-10-16 22:33:07 +0400

    {"types":"C","runs":23}

--- problem20.java
+++ problem20.java
@@ -1,4 +1,12 @@
-
-public class problem20 {
-
+import acm.program.ConsoleProgram;
+public class problem20 extends ConsoleProgram{
+    public void run(){
+    	int n = readInt("ENTER THE QUANTITY OF NUMBERS: ");
+    	int ans = 0;
+    	for(int i = 0; i < n; i++){
+    		int m = readInt("ENTER THE NUMBER: ");
+    		ans = ans + m;
+    	}
+    	print("THE ANSWER IS: " + ans);
+    }
 }
