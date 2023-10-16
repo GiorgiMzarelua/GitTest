3d7528b3913e900eb25d11d49bec2a20f169a606
S <s>
2023-10-16 21:54:38 +0400

    {"types":"C","runs":2}

--- problem15.java
+++ problem15.java
@@ -1,4 +1,10 @@
+import acm.program.ConsoleProgram;
 
-public class problem15 {
-
+public class problem15 extends ConsoleProgram{
+    public void run(){
+    	int n = readInt("ENTER THE FIRST NUMBER: " );
+    	int m = readInt("ENTER THE FIRST NUMBER: " );
+    	double x = (n + m) / 2;
+    	print("THE ANSWER IS: " + x);
+    }
 }
