81a533e2ab3c26b13d83e5697df48b9c4427ce62
S <s>
2023-10-17 00:06:20 +0400

    {"types":"C","runs":47}

--- problem26.java
+++ problem26.java
@@ -1,4 +1,23 @@
+import acm.program.ConsoleProgram;
 
-public class problem26 {
+public class problem26 extends ConsoleProgram{
+	public void run(){
+	    int n = readInt("ENTER THE FIRST NUMBER: ");
+		int m = readInt("ENTER THE SECOND NUMBER: ");
+	   	for(int i = min(n,m); i >= 1; i--){
+	   		if(m % i == 0 & n % i == 0){
+	   			print("THIS IS THE ANSWER: " + i);
+	   			break;
+	   		}
+	   	}
+	}
 
+	private int min(int n, int m) {
+		if(n < m){
+			return n;
+		}
+		else {
+			return m;
+		}
+	}
 }
