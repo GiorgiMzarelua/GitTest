8be791c034a4e89fa252bbd54f080b39eb6a7111
S <s>
2023-10-16 22:00:52 +0400

    {"types":"C","runs":8}

--- problem16.java
+++ problem16.java
@@ -1,4 +1,11 @@
+import acm.program.ConsoleProgram;
 
-public class problem16 {
-
+public class problem16 extends ConsoleProgram{
+    public void run(){
+    	double a = readInt("ENTER THE FIRST NUMBER: ");
+    	double b = readInt("ENTER THE SECOND NUMBER: ");
+    	double c = readInt("ENTER THE THIRD NUMBER: ");
+    	double ans = 1 / ((1 / a) + (1 / b) + (1 / c));
+    	print("THE ANSWER IS: " + ans);
+    }
 }
