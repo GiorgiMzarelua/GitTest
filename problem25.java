b8ab9010582c545f2e9ef12c97fd479becedffde
S <s>
2023-10-17 00:01:11 +0400

    {"types":"C","runs":43}

--- problem25.java
+++ problem25.java
@@ -4,7 +4,7 @@ public class problem25 extends ConsoleProgram{
 	public void run(){
 		int n = readInt("ENTER THE FIRST NUMBER: ");
 		int m = readInt("ENTER THE SECOND NUMBER: ");
-		for(int i = max(n, m); i < n * m; i++ ){
+		for(int i = max(n, m); i <= n * m; i++ ){
 			if(i % n == 0 & i % m == 0){
 				print("THIS IS THE ANSWER: " + i);
 				break;
