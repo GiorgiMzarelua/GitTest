9a8bef3e773ae80a51fdab97cb6ec55f80d4ede0
S <s>
2023-10-16 22:23:31 +0400

    {"types":"C","runs":17}

--- problem17.java
+++ problem17.java
@@ -7,8 +7,8 @@ public class problem17 extends ConsoleProgram{
         for(ans1 = 0; ans1 < num; ans1++){
         	
         }
-    	double ans2 = num - ans1;
-    	println("INTEGER PART OF THE NUMBER: " + ans1);
+    	double ans2 = num - ans1 + 1;
+    	println("INTEGER PART OF THE NUMBER: " + (ans1 -1));
     	print("FRACTIONAL PART OF THE NUMBER " + ans2);
     }
 }
