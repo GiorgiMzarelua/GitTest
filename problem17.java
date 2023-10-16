ab3cce0f24e48f5f81b1a47d6a26dad8690276c3
S <s>
2023-10-16 22:12:09 +0400

    {"types":"C","runs":11}

--- problem17.java
+++ problem17.java
@@ -1,4 +1,14 @@
+import acm.program.ConsoleProgram;
 
-public class problem17 {
-
+public class problem17 extends ConsoleProgram{
+    public void run(){
+    	double num = readInt("ENTER THE NUMBER: ");
+    	int ans1 = 0;
+        for(ans1 = 0; ans1 < num; ans1++){
+        	
+        }
+    	double ans2 = num - ans1;
+    	print("INTEGER PART OF THE NUMBER: " + ans1);
+    	print("FRACTIONAL PART OF THE NUMBER " + ans2);
+    }
 }
