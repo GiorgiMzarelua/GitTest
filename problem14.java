e677df88e27ebdfdb038d39eae4414118ef94f82
S <s>
2023-10-16 21:36:33 +0400

    {"types":"C","runs":26}

--- problem14.java
+++ problem14.java
@@ -39,7 +39,7 @@ public class problem14 extends GraphicsProgram{
 		int x2 = (int) (getWidth() / 2 + 70);
 		int y2 = (int) (getHeight() / 2  + 70);
 		eye1.setFilled(true);
-		eye1.setColor(Color.WHITE);
+		eye1.setColor(Color.YELLOW);
 		add(eye2, x2, y2);
 		
 	}
