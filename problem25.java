8f95209ee4f3b41d3363733390b0171113a7dd25
S <s>
2023-10-17 00:00:52 +0400

    {"types":"C","runs":42}

--- problem25.java
+++ problem25.java
@@ -1,4 +1,23 @@
+import acm.program.ConsoleProgram;
 
-public class problem25 {
+public class problem25 extends ConsoleProgram{
+	public void run(){
+		int n = readInt("ENTER THE FIRST NUMBER: ");
+		int m = readInt("ENTER THE SECOND NUMBER: ");
+		for(int i = max(n, m); i < n * m; i++ ){
+			if(i % n == 0 & i % m == 0){
+				print("THIS IS THE ANSWER: " + i);
+				break;
+			}
+		}
+	}
 
+	private int max(int n, int m) {
+		if(n > m){
+			return n;
+		}
+		else{
+			return m;
+		}
+	}
 }
