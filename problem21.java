c816d6435a4508d2f1e435ab97a5c61f40554cbf
S <s>
2023-10-16 23:27:03 +0400

    {"types":"C","runs":24}

--- problem21.java
+++ problem21.java
@@ -1,4 +1,34 @@
+import acm.graphics.GLine;
+import acm.program.GraphicsProgram;
+public class problem21 extends GraphicsProgram{
+    public void run(){
+    	drawTable();
+    }
 
-public class problem21 {
+	private void drawTable() {
+		drawRows();
+		drawColumns();
+	}
 
+	private void drawColumns() {
+		for(int i = 1; i <= 9; i++){
+			double x1 = getWidth() * i / 10;
+			double y1 = 0;
+			double x2 = getWidth() * i / 10;
+			double y2 = getHeight();
+			GLine line = new GLine(x1, y1, x2, y2);
+			add(line);
+		}
+	}
+
+	private void drawRows() {
+		for(int i = 1; i <= 9; i++){
+			double x1 = 0;
+			double y1 = getHeight() * i / 10;
+			double x2 = getWidth();
+			double y2 = getHeight() * i / 10;
+			GLine line = new GLine(x1, y1, x2, y2);
+			add(line);
+		}
+	}
 }
