d8ad5214d455d36d8c54b1ca2869b157dddaa0e6
S <s>
2023-10-17 17:19:09 +0400

    {"types":"C","runs":4}

--- problem28.java
+++ problem28.java
@@ -1,4 +1,15 @@
+import acm.program.ConsoleProgram;
 
-public class problem28 {
-
+public class problem28 extends ConsoleProgram{
+    public void run(){
+    	int n = readInt ("ENTER THE NUMBER: ");
+    	int ans = 0;
+    	while (n != -1){
+    		n = readInt("ENTER THE NUMBER: ");
+    		if(n % 2 == 0 ){
+    		    ans++;
+    		}
+    	}
+    	print("THIS IS THE ANSWER: " + ans);
+    }
 }
