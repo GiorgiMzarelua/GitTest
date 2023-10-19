e35f8f091a1c43008ed5914393ba3dbeeb27a626
S <s>
2023-10-19 13:17:11 +0400

    {"types":"A","runs":2}

---
+++ test.java
@@ -0,0 +1,11 @@
+import java.awt.Color;
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
+
+public class test extends GraphicsProgram{
+	public void run(){
+		GRect rect = new GRect(20, 30);
+		rect.setFilled(true);
+		add(rect, 100, 100);
+	}
+}
