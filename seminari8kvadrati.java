71d702c130f15380ca5f8e75dca18b4c99d5c6dc
S <s>
2023-10-24 17:17:35 +0400

    {"types":"C","runs":1}

--- seminari8kvadrati.java
+++ seminari8kvadrati.java
@@ -1,4 +1,17 @@
+import acm.program.GraphicsProgram;
+import acm.graphics.GRect;
+import acm.program.ConsoleProgram;
 
-public class seminari8kvadrati {
+public class seminari8kvadrati extends GraphicsProgram{
+	public void run(){
+		int size = readInt("enter the size of a side of a square: ");
+		drawTheSquare(size);
+	}
 
+	private void drawTheSquare(int size) {
+		GRect square = new GRect(size, size);
+		int x = getWidth() / 2 - size / 2;
+		int y = getHeight() / 2 - size / 2;
+		add(square);
+	}
 }
