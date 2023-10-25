118b9c0b64acda16af1273aba4e8e2aa482ae088
S <s>
2023-10-26 00:45:01 +0400

    {"types":"C","runs":1}

--- problem30.java
+++ problem30.java
@@ -2,29 +2,29 @@ import acm.program.ConsoleProgram;
 
 public class problem30 extends ConsoleProgram{
     public void run(){
-    	int n = readInt("ENTER THE NUMBER: ");
-    	int m = n;
+    	int m = readInt("ENTER THE NUMBER: ");
     	quantityOfDigits(m);
     	
     }
 
-	private void quantityOfDigits(int m) {
+	private int quantityOfDigits(int m) {
 		int amount = 0;
 		int a = m;
 		while(m > 0){
 			m = m / 10;
 			amount ++;
 		}
-		conjuction(a, amount);
+		return conjuction(a, amount);
 	}
 
-	private void conjuction(int a, int amount) {
+	private int conjuction(int a, int amount) {
+		int b = 0;
 		while(a > 0){
 			for(int i = amount; i >= 1; i--){
-				
+				b = 10 * b + a % 10;
+				a = a / 10;
 			}
-			int b = a % 10;
-			a = a / 10;
 		}
+		return b;
 	}
 }
