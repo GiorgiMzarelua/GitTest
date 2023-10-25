69dbc73fe76d4119ebc3367f6a638d7f48cc537d
S <s>
2023-10-26 00:59:05 +0400

    {"types":"C","runs":16}

--- problem32.java
+++ problem32.java
@@ -1,4 +1,11 @@
-
-public class problem32 {
-
+import acm.program.ConsoleProgram;
+public class problem32 extends ConsoleProgram{
+    public void run(){
+    	int counter  = 0;
+    	for(int i = 1; i < 1000000; i++){
+    		for(int j = 2; j < i; j++){
+    			if 
+    		}
+    	}
+    }
 }
