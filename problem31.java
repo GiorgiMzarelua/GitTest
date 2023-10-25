1d393809f4cfd84401010124eb8cc9277d4e0a11
S <s>
2023-10-26 00:51:12 +0400

    {"types":"C","runs":6}

--- problem31.java
+++ problem31.java
@@ -1,4 +1,19 @@
+import acm.program.ConsoleProgram;
 
-public class problem31 {
-
+public class problem31 extends ConsoleProgram{
+    public void run(){
+    	int n = readInt("ENTER THE NUMBER: ");
+    	if(n == 2){
+    		println("THIS IS A PRIME NUMBER.");
+    	}
+    	for(int i = 2; i < n; i++){
+    		if(n % i == 0){
+    			println("THIS IS NOT A PRIME NUMBER.");
+    			break; 
+    		}
+    		else{
+    			println("THIS IS A PRIME NUMBER.");
+    		}
+    	}
+    }
 }
