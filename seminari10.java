9c7d8322c04d63a10e7c90ddf8ed341d5ac9d560
S <s>
2023-10-26 17:39:34 +0400

    {"types":"C","runs":1}

--- seminari10.java
+++ seminari10.java
@@ -1,4 +1,18 @@
+import acm.graphics.GPolygon;
+import acm.program.GraphicsProgram;
+/*
+ * GraphicsProgram ში დაწერეთ მეთოდი, რომელსაც გადაეცემა გვერდის სიგრძე და რომელიც აბრუნებს წესიერ სამკუთხედს როგორც
+ * ობიექტს(GPolygon შეგიძლიათ გამოიყენოთ) დაბრუნებული სამკუთხედის კოორდინატი იყოს 0.0 (შემოხაზული მართკუთხედის 
+ * ზედა მარცხენა კუთხიის კოორდინატი). მაგრამ უნდა მუშაობდეს add(triangle, x, y) ფუნქციონალიც.
+ */
+public class seminari10 extends GraphicsProgram{
+	public void run(){
+		int size = 100;
+		drawAtriangle(size);
+	}
 
-public class seminari10 {
-
+	private void drawAtriangle(int size) {
+		GPolygon triangle = new GPolygon();
+		triangle.addVertex(0, size);
+	}
 }
