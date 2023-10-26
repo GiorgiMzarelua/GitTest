6bc01b80b32ddfe0f2f8c3c503806581cd40cbdf
S <s>
2023-10-26 17:48:41 +0400

    {"types":"C","runs":9}

--- seminar10i2.java
+++ seminar10i2.java
@@ -1,4 +1,12 @@
-
-public class seminar10i2 {
-
+import acm.program.GraphicsProgram;
+/*
+ * (დაწერილი უნდა გქონდეთ problem set-ის ამოცანა 22- ჭადრაკის დაფა) გადააკეთეთ თქვენი კოდი ისე, რომ შექმნილი ჭადრაკის
+ *  დაფა წარმოადგენდეს GCompound ობიექტს. დაწერეთ მეთოდი:private GCompound chessBoard(double cellLength, 
+ *  double numCells)run-ში კი გამოიძახეთ ეს მეთოდი, მისი გამოყენებით შექმენით GCompound board ობიექტი
+ *   და დაამატეთ 50, 100 კოორდინატებზე.
+ */
+public class seminar10i2 extends GraphicsProgram{
+	public void run(){
+	    
+	}
 }
