9af8fe8336710b8660e9d6832ec91c5029d2ce3c
S <s>
2023-10-26 18:00:32 +0400

    {"types":"C","runs":9}

--- seminar10i2.java
+++ seminar10i2.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 /*
  * (დაწერილი უნდა გქონდეთ problem set-ის ამოცანა 22- ჭადრაკის დაფა) გადააკეთეთ თქვენი კოდი ისე, რომ შექმნილი ჭადრაკის
@@ -7,6 +9,85 @@ import acm.program.GraphicsProgram;
  */
 public class seminar10i2 extends GraphicsProgram{
 	public void run(){
-	    
+		for(int i = 0; i < 4; i++){
+			startWithBlackRow();
+			startWithWhiteRow();
+		}
+	}
+
+	private void startWithWhiteRow() {
+		for(int i = 0; i < 4; i++){
+			GRect rec = new GRect(getWidth() / 8, getHeight() / 8);
+			double x = (double) (getWidth() *  i / 4 + getWidth() / 8);
+			double y = (double) (getHeight() / 8);
+			rec.setFilled(true);
+			rec.setColor(Color.BLACK);
+			add(rec, x, y);
+		}
+		
+		for(int i = 0; i < 4; i++){
+			GRect rec = new GRect(getWidth() / 8, getHeight() / 8);
+			double x = (double) (getWidth() *  i / 4 + getWidth() / 8);
+			double y = (double) (getHeight() * 3 / 8);
+			rec.setFilled(true);
+			rec.setColor(Color.BLACK);
+			add(rec, x, y);
+		}
+		
+		for(int i = 0; i < 4; i++){
+			GRect rec = new GRect(getWidth() / 8, getHeight() / 8);
+			double x = (double) (getWidth() *  i / 4 + getWidth() / 8);
+			double y = (double) (getHeight() * 5 / 8);
+			rec.setFilled(true);
+			rec.setColor(Color.BLACK);
+			add(rec, x, y);
+		}
+		
+		for(int i = 0; i < 4; i++){
+			GRect rec = new GRect(getWidth() / 8, getHeight() / 8);
+			double x = (double) (getWidth() *  i / 4 + getWidth() / 8);
+			double y = (double) (getHeight() * 7 / 8);
+			rec.setFilled(true);
+			rec.setColor(Color.BLACK);
+			add(rec, x, y);
+		}
+	}
+
+	private void startWithBlackRow() {
+		for(int i = 0; i < 4; i++){
+			GRect rec = new GRect(getWidth() / 8, getHeight() / 8);
+			double x = (double) (getWidth() *  i / 4);
+			double y = (double) (0);
+			rec.setFilled(true);
+			rec.setColor(Color.BLACK);
+			add(rec, x, y);
+		}
+		
+		for(int i = 0; i < 4; i++){
+			GRect rec = new GRect(getWidth() / 8, getHeight() / 8);
+			double x = (double) (getWidth() *  i / 4);
+			double y = (double) (getHeight() / 4);
+			rec.setFilled(true);
+			rec.setColor(Color.BLACK);
+			add(rec, x, y);
+		}
+		
+		for(int i = 0; i < 4; i++){
+			GRect rec = new GRect(getWidth() / 8, getHeight() / 8);
+			double x = (double) (getWidth() *  i / 4);
+			double y = (double) (getHeight() / 2);
+			rec.setFilled(true);
+			rec.setColor(Color.BLACK);
+			add(rec, x, y);
+		}
+		
+		for(int i = 0; i < 4; i++){
+			GRect rec = new GRect(getWidth() / 8, getHeight() / 8);
+			double x = (double) (getWidth() *  i / 4);
+			double y = (double) (getHeight() * 3 / 4);
+			rec.setFilled(true);
+			rec.setColor(Color.BLACK);
+			add(rec, x, y);
+		}
 	}
 }
