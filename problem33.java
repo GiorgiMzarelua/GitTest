33695491b08fcc12e49c63ffa71cabaf52d05681
S <s>
2023-10-31 17:26:09 +0400

    {"types":"C","runs":1}

--- problem33.java
+++ problem33.java
@@ -1,4 +1,10 @@
-
-public class problem33 {
-
+import acm.program.GraphicsProgram;
+import acm.util.*;
+public class problem33 extends GraphicsProgram{
+	public static final int STARTING_RANGE = 100;
+	public static final int FINAL_RANGE = 200;
+    public void run(){
+    	print(rgen);
+    }
+    private RandomGenerator rgen = RandomGenerator.getInstance();
 }
