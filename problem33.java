a683d1dcb4df448a28f081fc40ff4de83d8c2478
S <s>
2023-10-31 17:48:16 +0400

    {"types":"C","runs":5}

--- problem33.java
+++ problem33.java
@@ -1,10 +1,17 @@
-import acm.program.ConsoleProgram;
+import java.awt.Color;
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
 import acm.util.*;
-public class problem33 extends ConsoleProgram{
+public class problem33 extends GraphicsProgram{
 	public static final int STARTING_RANGE = 100;
 	public static final int FINAL_RANGE = 200;
     public void run(){
-    	print(rgen);
+    	RandomGenerator rgen = new RandomGenerator();
+    	int radius =  rgen.nextInt(STARTING_RANGE, FINAL_RANGE);
+    	GOval circle = new GOval(getWidth() / 2 - radius, getHeight() / 2 - radius, radius, radius);
+    	circle.setFilled(true);
+    	Color randColor = rgen.nextColor();
+    	circle.setColor(randColor);
+    	add(circle);
     }
-    private RandomGenerator rgen = RandomGenerator.getInstance(STARTING_RANGE, FINAL_RANGE);
 }
