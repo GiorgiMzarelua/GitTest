696c600567743923ef0c32acca14ab3441ec9f6e
S <s>
2023-10-31 17:26:45 +0400

    {"types":"C","runs":2}

--- problem33.java
+++ problem33.java
@@ -1,6 +1,6 @@
-import acm.program.GraphicsProgram;
+import acm.program.ConsoleProgram;
 import acm.util.*;
-public class problem33 extends GraphicsProgram{
+public class problem33 extends ConsoleProgram{
 	public static final int STARTING_RANGE = 100;
 	public static final int FINAL_RANGE = 200;
     public void run(){
