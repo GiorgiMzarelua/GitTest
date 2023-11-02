d902d9433a4b12d0e7b0dafb333cbcdadd1a47f2
S <s>
2023-11-02 20:22:10 +0400

    {"types":"C","runs":27}

--- problem35.java
+++ problem35.java
@@ -1,4 +1,18 @@
+import acm.program.ConsoleProgram;
 
-public class problem35 {
-
+public class problem35 extends ConsoleProgram{
+    private static final int NUM_EXPERIMENTS = 10000;
+    public void run(){
+    	double sumFlips = 0;
+    	for(int i = 0; i < NUM_EXPERIMENTS; i++){
+    		int currFlips = holdExperiment();
+    		sumFlips += currFlips;
+    	}
+    	double avgFlips = sumFlips / NUM_EXPERIMENTS;
+        println(avgFlips);
+    }
+	private int holdExperiment() {
+		
+		return 0;
+	}
 }
