e813c99159d1925f9ac2bdccd6bd5d00abca82d9
S <s>
2023-11-07 17:44:32 +0400

    {"types":"C","runs":1}

--- problem37.java
+++ problem37.java
@@ -1,4 +1,11 @@
+import acm.program.ConsoleProgram;
+import acm.util.RandomGenerator;
 
-public class problem37 {
-
+public class problem37 extends ConsoleProgram{
+	private static final int MIN_VALUE = 0;
+	private static final int MAX_VALUE = 36;
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+    public void run(){
+    	
+    }
 }
--- problem39.java
+++ problem39.java
@@ -1,4 +1,17 @@
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
 
-public class problem39 {
+public class problem39 extends GraphicsProgram{
+	private static final int PAUSE_TIME = 20;
+	private static final int INITIAL_VELOCITY = 4;
+	private static final int RADIUS = 15;
+	public void run(){
+		drawCircle();
+	}
+	private void drawCircle() {
+		GOval circle = new GOval(0, getHeight() / 2 - RADIUS, 2 * RADIUS, 2 * RADIUS);
+		circle.setFilled(true);
+		add(circle);
+	}
 
 }
