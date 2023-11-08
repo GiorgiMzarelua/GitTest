22c699870ced92ffb1ce2b4ca4937ec895aa1c0a
S <s>
2023-11-08 18:30:01 +0400

    {"types":"C","runs":17}

--- problem36.java
+++ problem36.java
@@ -2,9 +2,33 @@ import acm.program.ConsoleProgram;
 import acm.util.RandomGenerator; 
 
 public class problem36 extends ConsoleProgram{
-	private static final int NUM_EXPERIMENTS = 10000;
-	
+	private static final int NUM_EXPERIMENTS = 100000;
+	private RandomGenerator rgen = RandomGenerator.getInstance();
     public void run(){
-    	
+    	int sumFlips = 0;
+    	for(int i = 0; i < NUM_EXPERIMENTS; i++){
+    		int currFlips = holdExperiment();
+    		sumFlips += currFlips;
+    	}
+    	int avgFlips = sumFlips / NUM_EXPERIMENTS;
+    	println("this is the average number of getting head three times in a row: " + avgFlips);
     }
+	private int holdExperiment() {
+		int numFlips = 0;
+		int headsInArow = 0;
+		while(true){
+			numFlips++;
+			boolean isHeads = rgen.nextBoolean(0.4);
+			if(isHeads){
+				headsInArow++;
+				if(headsInArow == 3){
+					break;
+				}
+				else{
+					headsInArow = 0;
+				}
+			}
+		}
+		return numFlips;
+	}
 }
