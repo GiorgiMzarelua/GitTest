ca1ca81183c80603b950d146f7b068776c80a06f
S <s>
2023-11-08 21:32:57 +0400

    {"types":"C","runs":42}

--- additional13seminari2.java
+++ additional13seminari2.java
@@ -6,13 +6,14 @@ import acm.program.GraphicsProgram;
 public class additional13seminari2 extends GraphicsProgram{
 	private static final int RADIUS = 25;
 	public void run(){
-		drawCircle();
+		GOval circle = drawCircle();
+		
 	}
-	private void drawCircle() {
+	private GOval drawCircle() {
 		GOval circle = new GOval(getWidth() / 2 - RADIUS, 0, 2 * RADIUS, 2* RADIUS);
 		circle.setFilled(true);
 		circle.setColor(Color.RED);
-		add(circle);
+		return circle;
 	}
 
 }
