6ed5e6d3e1d0ce3147cd8d04869d9e407cf4652f
S <s>
2023-11-08 17:59:10 +0400

    {"types":"C","runs":7}

--- problem40.java
+++ problem40.java
@@ -3,7 +3,7 @@ import acm.program.GraphicsProgram;
 
 public class problem40 extends GraphicsProgram{
 	private static final int PAUSE_TIME = 20;
-	private static final int INITIAL_VELOCITY = 44;
+	private static final int INITIAL_VELOCITY = 4;
 	private static final int RADIUS = 25;
 	public void run(){
 		GOval circle = new GOval(0, getHeight() / 2 - RADIUS, 2 * RADIUS, 2 * RADIUS);
