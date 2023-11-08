f12b671e06a01f0ca37c23088c1d7175bfe24e0f
S <s>
2023-11-08 18:43:00 +0400

    {"types":"C","runs":27}

--- problem37.java
+++ problem37.java
@@ -6,6 +6,12 @@ public class problem37 extends ConsoleProgram{
 	private static final int MAX_VALUE = 36;
 	private RandomGenerator rgen = RandomGenerator.getInstance();
     public void run(){
-    	
+    	int bet = readInt("Enter the number: ");
+    	int roulleteResult = spinRoullete();
     }
+	private int spinRoullete() {
+		int result = rgen.nextInt(MIN_VALUE, MAX_VALUE);
+		println(result);
+		return result;
+	}
 }
