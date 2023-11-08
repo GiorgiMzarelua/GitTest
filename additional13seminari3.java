e7028c7295dca4f153876f457720f87a51db0d35
S <s>
2023-11-08 22:13:06 +0400

    {"types":"C","runs":62}

--- additional13seminari3.java
+++ additional13seminari3.java
@@ -1,4 +1,20 @@
+import java.awt.Color;
 
-public class additional13seminari3 {
-
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
+public class additional13seminari3 extends GraphicsProgram{
+	private static final int RADIUS = 35;
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+    public void run(){
+        GOval randomBall = drawBall();
+    }
+	private GOval drawBall() {
+		GOval circle = new GOval(getWidth() / 2 - RADIUS, getHeight() / 2 - RADIUS, 2 * RADIUS, 2 * RADIUS);
+		circle.setFilled(true);
+		Color color = rgen.nextColor();
+		circle.setColor(color);
+		add(cirlce);
+		return circle;
+	}
 }
