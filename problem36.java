4182e88b7ab77998a0764eba7858de14b5aedd21
S <s>
2023-11-08 18:31:20 +0400

    {"types":"C","runs":20}

--- problem36.java
+++ problem36.java
@@ -2,7 +2,7 @@ import acm.program.ConsoleProgram;
 import acm.util.RandomGenerator; 
 
 public class problem36 extends ConsoleProgram{
-	private static final int NUM_EXPERIMENTS = 100000;
+	private static final int NUM_EXPERIMENTS = 10000;
 	private RandomGenerator rgen = RandomGenerator.getInstance();
     public void run(){
     	int sumFlips = 0;
