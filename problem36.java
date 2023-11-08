5d9c6b36aec087a3d4d6afba3b7d617afde2ffe6
S <s>
2023-11-08 18:18:05 +0400

    {"types":"C","runs":16}

--- problem36.java
+++ problem36.java
@@ -1,4 +1,10 @@
+import acm.program.ConsoleProgram;
+import acm.util.RandomGenerator; 
 
-public class problem36 {
-
+public class problem36 extends ConsoleProgram{
+	private static final int NUM_EXPERIMENTS = 10000;
+	
+    public void run(){
+    	
+    }
 }
