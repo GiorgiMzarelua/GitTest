05cb1bdc419e87290dd37e9ee5f239900f5787bd
S <s>
2023-11-08 19:06:09 +0400

    {"types":"C","runs":33}

--- problem38.java
+++ problem38.java
@@ -5,12 +5,36 @@ public class problem38 extends ConsoleProgram{
 	private static final int MIN_VALUE = 0;
 	private static final int MAX_VALUE = 36;
 	private static final int STARTING_MONEY = 1000;
+	private RandomGenerator rgen = RandomGenerator.getInstance();
 	public void run(){
 		int currMoney = STARTING_MONEY;
 		while(currMoney >= 0){
 			int betNumber = filterNumbers();
+			int roulleteNumber = spinRoullete();
+			int bet = filterMoney(currMoney);
+			if(betNumber == roulleteNumber){
+				currMoney = currMoney * 2;
+			}
+			else{
+				currMoney = currMoney - betNumber;
+			}
 		}
 	}
+	
+	private int filterMoney(int currMoney) {
+		while(true){
+			int BET = readInt("Enter the number from " + 1 + " to " + currMoney + ": ");
+			if(BET >= 1 && BET <= currMoney){
+				return BET;
+			}
+		}
+	}
+
+	private int spinRoullete() {
+		int result = rgen.nextInt(MIN_VALUE, MAX_VALUE);
+		return result;
+	}
+
 	private int filterNumbers() {
 		while(true){
 			int Num = readInt("Enter the number from " + MIN_VALUE + " to " + MAX_VALUE + ": ");
@@ -19,5 +43,4 @@ public class problem38 extends ConsoleProgram{
 			}
 		}
 	}
-
 }
