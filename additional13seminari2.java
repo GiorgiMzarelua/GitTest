91ae416efcc0511a16ee0ce22a9dc92b6209aba1
S <s>
2023-11-09 09:51:19 +0400

    {"types":"C","runs":3}

--- additional13seminari2.java
+++ additional13seminari2.java
@@ -10,17 +10,17 @@ public class additional13seminari2 extends GraphicsProgram{
 	private static final int SPEED_DECREASE = 15;
 	public void run(){
 		GOval circle = drawCircle();
-	    int speed = 1; 
+	    double speed = 1; 
 		while(speed > 0 && circle.getY() <= getHeight() - 2 * RADIUS){
 			if(speed <= ACCELERATION){
 				while(circle.getY() <= getHeight() - 2 * RADIUS){
 					circle.move(0, speed);
 					pause(PAUSE_TIME);
-					if(speed <= getHeight() - circle.getY() ){
+					if(speed <= getHeight() - circle.getY()){
 						speed = speed + ACCELERATION;
 					}
 					else{
-						speed = getHeight() - speed - 2 * RADIUS;
+						speed = getHeight() - circle.getY() - 2 * RADIUS;
 					}
 				}
 			}
