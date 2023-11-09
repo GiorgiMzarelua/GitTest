c0e3b2c918b422ab95a611127c237a3d4a221f24
S <s>
2023-11-09 10:22:55 +0400

    {"types":"C","runs":19}

--- additional12seminari.java
+++ additional12seminari.java
@@ -10,20 +10,24 @@ public class additional12seminari extends GraphicsProgram{
 	private static final int PAUSE_TIME = 1000;
 	private RandomGenerator rgen = RandomGenerator.getInstance();
     public void run(){
-    	for(int i = 0; i < NUM_EXPERIMENTS; i++){
-    		GOval ball = drawCircle();
+    	int RED = 0;
+    	int GREEN = 0;
+    	for(int i = 0; i < NUM_EXPERIMENTS; i++){ 
+    		GOval ball = drawCircle(RED, GREEN);
     		pause(PAUSE_TIME);
     	}
     }
-	private GOval drawCircle() {
+	private GOval drawCircle(int RED, int GREEN) {
 		GOval circle = new GOval(getWidth() / 2 - RADIUS, getHeight() / 2 - RADIUS, 2 * RADIUS, 2 * RADIUS);
 		int k = rgen.nextInt(0, 1);
 		circle.setFilled(true);
 		if(k == 0){
 			circle.setColor(Color.RED);
+			RED++;
 		}
 		else{
 			circle.setColor(Color.GREEN);
+			GREEN++;
 		}
 		add(circle);
 		if(k == 0){
@@ -35,8 +39,8 @@ public class additional12seminari extends GraphicsProgram{
 		return circle;
 	}
 	private void drawTails() {
-		double x = getWidth() / 2 - 20;
-		double y = getHeight() / 2 - 5;
+		double x = getWidth() / 2 - 15;
+		double y = getHeight() / 2 - 2;
 		add(new GLabel("TAILS", x, y));
 		
 	}
