5b77821f16fd605e888393336ee7a6b02ed72d6d
S <s>
2023-11-09 17:50:18 +0400

    {"types":"C","runs":2}

--- problem41.java
+++ problem41.java
@@ -1,4 +1,22 @@
+import java.awt.Color;
+import java.awt.event.MouseEvent;
 
-public class problem41 {
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
 
+public class problem41 extends GraphicsProgram{
+	private static final int RADIUS = 15;
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	public void run(){
+		addMouseListeners();
+		
+	}
+	public void mouseClicked(MouseEvent e){
+		GOval circle = new GOval(2 * RADIUS, 2 * RADIUS);
+		circle.setFilled(true);
+		Color color = rgen.nextColor();
+		circle.setColor(color);
+		add(circle, e.getX(), e.getY());
+	}
 }
