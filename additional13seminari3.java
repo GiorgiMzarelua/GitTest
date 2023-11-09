6363aa75a9222b67e53103101a7465229d761032
S <s>
2023-11-09 19:01:06 +0400

    {"types":"C","runs":3}

--- additional13seminari3.java
+++ additional13seminari3.java
@@ -50,11 +50,11 @@ public class additional13seminari3 extends GraphicsProgram{
             	}
         	}
         	else{
-        		while(randomBall.getX() >= 0 && randomBall.getX() <= getWidth() - 2 * RADIUS  && randomBall.getY() >= 0 && randomBall.getY() <= getHeight() - 2 * RADIUS ) 
+        		while(randomBall.getX() >= a && randomBall.getX() <= getWidth() - 2 * RADIUS - b && randomBall.getY() >= a && randomBall.getY() <= getHeight() - 2 * RADIUS - b) 
             	{
             		randomBall.move(a, b);
         			pause(PAUSE_TIME);
-            	}
+            	} 
         	}
         }
     }
