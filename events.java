a4536283e277f25069f5b6da830aa44cd548c00b
S <s>
2023-11-09 17:16:57 +0400

    {"types":"C","runs":1}

--- events.java
+++ events.java
@@ -1,4 +1,27 @@
+import java.awt.Color;
+import java.awt.event.MouseEvent;
 
-public class events {
+import acm.graphics.GObject;
+import acm.graphics.GRect;
+import acm.program.GraphicsProgram;
 
+public class events extends GraphicsProgram{
+    public void run(){
+    	GRect rect = new GRect(100, 100);
+    	rect.setFilled(true);
+    	add(rect);
+    	
+    	GRect rect2 = new GRect(100, 100);
+    	rect2.setFilled(true);
+    	add(rect2, 200, 200);
+    	
+    	addMouseListeners();
+    }
+    public void mouseClicked(MouseEvent e){
+//    	e.getY();
+    	GObject obj = getElementAt(e.getX(), e.getY());
+    	if(obj != null){
+    		obj.setColor(Color.RED);
+    	}
+    }
 }
