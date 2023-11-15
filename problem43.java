0640a8c963002dac01e06c8ff4a0e2bae8c7347b
S <s>
2023-11-15 22:14:53 +0400

    {"types":"C","runs":48}

--- problem43.java
+++ problem43.java
@@ -6,19 +6,22 @@ import acm.graphics.GOval;
 import acm.program.GraphicsProgram;
 
 public class problem43 extends GraphicsProgram{
-	private static final int RADIUS = 30;
+	private static final int RADIUS = 70;
 	private GObject lastPressedObj;
 	private int prevX;
 	private int prevY;
     public void run(){
-    	GOval circle = new GOval(2 * RADIUS, 2 * RADIUS);
+    	addCircle();
+    	addMouseListeners();
+    }
+	private void addCircle() {
+		GOval circle = new GOval(2 * RADIUS, 2 * RADIUS);
 		int x = getWidth() / 2 - RADIUS;
 		int y = getHeight() / 2 - RADIUS;
 		circle.setFilled(true);
 		circle.setColor(Color.BLACK);
 		add(circle, x, y);
-    	addMouseListeners();
-    }
+	}
 	
 	public void MousePressed(MouseEvent e){
 		GObject obj = getElementAt(e.getX(), getY());
@@ -37,3 +40,42 @@ public class problem43 extends GraphicsProgram{
 		}
 	}
 }
+//import java.awt.event.MouseEvent;
+//import acm.graphics.*;
+//import acm.program.GraphicsProgram;
+//
+//public class Problem43 extends GraphicsProgram {
+//	private static final int RADIUS = 50;
+//	private GObject lastPressedObj;
+//	private int prevX;
+//	private int prevY;
+//	
+//	public void run() {
+//		GOval circle = new GOval(2 * RADIUS, 2 * RADIUS);
+//		circle.setFilled(true);
+//		add(circle, getWidth() / 2 - RADIUS, getHeight() / 2 - RADIUS);
+//		addMouseListeners();
+//	}
+//	
+//	public void mousePressed(MouseEvent e) {
+//		GObject obj = getElementAt(e.getX(), e.getY());
+//		lastPressedObj = obj;
+//		if(obj != null) {
+//			// update values
+//			prevX = e.getX();
+//			prevY = e.getY();
+//		}
+//	}
+//	
+//	public void mouseDragged(MouseEvent e) {
+//		if(lastPressedObj != null) {
+//			// move object
+//			lastPressedObj.move(e.getX() - prevX, e.getY() - prevY);
+//			// update values
+//			prevX = e.getX();
+//			prevY = e.getY();
+//		}
+//	}
+//
+//}
+//
