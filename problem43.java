03a5b051b47ee4f0f5c9deee24b51199334a72bf
S <s>
2023-11-15 22:06:39 +0400

    {"types":"C","runs":45}

--- problem43.java
+++ problem43.java
@@ -1,11 +1,15 @@
 import java.awt.Color;
 import java.awt.event.MouseEvent;
 
+import acm.graphics.GObject;
 import acm.graphics.GOval;
 import acm.program.GraphicsProgram;
 
 public class problem43 extends GraphicsProgram{
 	private static final int RADIUS = 30;
+	private GObject lastPressedObj;
+	private int prevX;
+	private int prevY;
     public void run(){
     	addCircle();
     	addMouseListeners();
@@ -19,7 +23,20 @@ public class problem43 extends GraphicsProgram{
 		add(circle, x, y);
 	}
 	
+	public void MousePressed(MouseEvent e){
+		GObject obj = getElementAt(e.getX(), getY());
+		lastPressedObj = obj;
+		if(obj != null){
+			prevX = e.getX();
+			prevY = e.getY();
+		}
+	}
+	
 	public void MouseDragged(MouseEvent e){
-		
+		if(lastPressedObj != null){
+			lastPressedObj.move(e.getX() - prevX, e.getY() - prevY);
+			prevX = e.getX();
+			prevY = e.getY();
+		}
 	}
 }
