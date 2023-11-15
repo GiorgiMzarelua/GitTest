b309c0876c1ad13a1869f257244aeb68b5b637f3
S <s>
2023-11-15 22:07:36 +0400

    {"types":"C","runs":46}

--- problem43.java
+++ problem43.java
@@ -1,42 +1,81 @@
-import java.awt.Color;
-import java.awt.event.MouseEvent;
+//import java.awt.Color;
+//import java.awt.event.MouseEvent;
+//
+//import acm.graphics.GObject;
+//import acm.graphics.GOval;
+//import acm.program.GraphicsProgram;
+//
+//public class problem43 extends GraphicsProgram{
+//	private static final int RADIUS = 30;
+//	private GObject lastPressedObj;
+//	private int prevX;
+//	private int prevY;
+//    public void run(){
+//    	addCircle();
+//    	addMouseListeners();
+//    }
+//	private void addCircle() {
+//		GOval circle = new GOval(2 * RADIUS, 2 * RADIUS);
+//		int x = getWidth() / 2 - RADIUS;
+//		int y = getHeight() / 2 - RADIUS;
+//		circle.setFilled(true);
+//		circle.setColor(Color.BLACK);
+//		add(circle, x, y);
+//	}
+//	
+//	public void MousePressed(MouseEvent e){
+//		GObject obj = getElementAt(e.getX(), getY());
+//		lastPressedObj = obj;
+//		if(obj != null){
+//			prevX = e.getX();
+//			prevY = e.getY();
+//		}
+//	}
+//	
+//	public void MouseDragged(MouseEvent e){
+//		if(lastPressedObj != null){
+//			lastPressedObj.move(e.getX() - prevX, e.getY() - prevY);
+//			prevX = e.getX();
+//			prevY = e.getY();
+//		}
+//	}
+//}
 
-import acm.graphics.GObject;
-import acm.graphics.GOval;
+import java.awt.event.MouseEvent;
+import acm.graphics.*;
 import acm.program.GraphicsProgram;
 
-public class problem43 extends GraphicsProgram{
-	private static final int RADIUS = 30;
+public class problem43 extends GraphicsProgram {
+	private static final int RADIUS = 50;
 	private GObject lastPressedObj;
 	private int prevX;
 	private int prevY;
-    public void run(){
-    	addCircle();
-    	addMouseListeners();
-    }
-	private void addCircle() {
+	
+	public void run() {
 		GOval circle = new GOval(2 * RADIUS, 2 * RADIUS);
-		int x = getWidth() / 2 - RADIUS;
-		int y = getHeight() / 2 - RADIUS;
 		circle.setFilled(true);
-		circle.setColor(Color.BLACK);
-		add(circle, x, y);
+		add(circle, getWidth() / 2 - RADIUS, getHeight() / 2 - RADIUS);
+		addMouseListeners();
 	}
 	
-	public void MousePressed(MouseEvent e){
-		GObject obj = getElementAt(e.getX(), getY());
+	public void mousePressed(MouseEvent e) {
+		GObject obj = getElementAt(e.getX(), e.getY());
 		lastPressedObj = obj;
-		if(obj != null){
+		if(obj != null) {
+			// update values
 			prevX = e.getX();
 			prevY = e.getY();
 		}
 	}
 	
-	public void MouseDragged(MouseEvent e){
-		if(lastPressedObj != null){
+	public void mouseDragged(MouseEvent e) {
+		if(lastPressedObj != null) {
+			// move object
 			lastPressedObj.move(e.getX() - prevX, e.getY() - prevY);
+			// update values
 			prevX = e.getX();
 			prevY = e.getY();
 		}
 	}
+
 }
