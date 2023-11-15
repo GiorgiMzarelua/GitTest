99839b84eca37251067ee223ffa5f88d99e65194
S <s>
2023-11-15 21:56:04 +0400

    {"types":"C","runs":43}

--- problem43.java
+++ problem43.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.awt.event.MouseEvent;
 
 import acm.graphics.GOval;
 import acm.program.GraphicsProgram;
@@ -7,6 +8,7 @@ public class problem43 extends GraphicsProgram{
 	private static final int RADIUS = 30;
     public void run(){
     	addCircle();
+    	addMouseListeners();
     }
 	private void addCircle() {
 		GOval circle = new GOval(2 * RADIUS, 2 * RADIUS);
@@ -15,5 +17,9 @@ public class problem43 extends GraphicsProgram{
 		circle.setFilled(true);
 		circle.setColor(Color.BLACK);
 		add(circle, x, y);
+	}
+	
+	public void MouseDragged(MouseEvent e){
+		
 	}
 }
