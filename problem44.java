f8286947ebff79bdfb6281f8b00f33a467d41b73
S <s>
2023-11-15 22:32:53 +0400

    {"types":"C","runs":57}

--- problem44.java
+++ problem44.java
@@ -1,55 +1,26 @@
-//import java.awt.event.MouseEvent;
-//
-//import acm.graphics.GObject;
-//import acm.graphics.GOval;
-//import acm.program.GraphicsProgram;
-//
-//public class problem44 extends GraphicsProgram{
-//	private static final int RADIUS = 70;
-//	private static final int SPEED = 5;
-//	private static final int DELAY = 20;
-//	private GOval circle;
-//    public void run(){
-//    	addMouseListeners();
-//    	circle = new GOval(2 * RADIUS, 2 * RADIUS);
-//    	circle.setFilled(true);
-//    	add(circle);
-//    	while(true){
-//    		circle.move(0, SPEED);
-//    		pause(DELAY);
-//    	}
-//    }
-//    
-//    public void MouseClicked(MouseEvent e){
-//    	add(circle, e.getX() - RADIUS, e.getY() - RADIUS);
-//    }
-//}
 import java.awt.event.MouseEvent;
 
+import acm.graphics.GObject;
 import acm.graphics.GOval;
 import acm.program.GraphicsProgram;
 
-public class problem44 extends GraphicsProgram {
-	private static final int RADIUS = 50;
-	private static final int V_Y = 5;
+public class problem44 extends GraphicsProgram{
+	private static final int RADIUS = 70;
+	private static final int SPEED = 5;
 	private static final int DELAY = 20;
 	private GOval circle;
-
-	public void run() {
-		circle = new GOval(2 * RADIUS, 2 * RADIUS);
-		circle.setFilled(true);
-
-		addMouseListeners();
-
-		// while loop should always be in run(not in listener methods)
-		while (true) {
-			circle.move(0, V_Y);
-			pause(DELAY);
-		}
-	}
-
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		add(circle, e.getX() - RADIUS, e.getY() - RADIUS);
-	}
+    public void run(){
+    	circle = new GOval(2 * RADIUS, 2 * RADIUS);
+    	circle.setFilled(true);
+    	addMouseListeners();
+    	add(circle);
+    	while(true){
+    		circle.move(0, SPEED);
+    		pause(DELAY);
+    	}
+    }
+    
+    public void MouseClicked(MouseEvent e){
+    	add(circle, e.getX() - RADIUS, e.getY() - RADIUS);
+    }
 }
