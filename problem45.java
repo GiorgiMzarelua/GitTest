f803bbf3abd2a11a7393d9d6224e0599b8b7543c
S <s>
2023-11-15 22:46:00 +0400

    {"types":"C","runs":62}

--- problem45.java
+++ problem45.java
@@ -1,4 +1,8 @@
+import acm.program.ConsoleProgram;;
 
-public class problem45 {
+public class problem45 extends ConsoleProgram{
+    public void run(){
+    	String s = readString("Enter the text: ");
+    }
 
 }
