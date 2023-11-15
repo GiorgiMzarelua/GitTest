c56df400b45257ba6a3c0a3d5b75e8188cf163d0
S <s>
2023-11-15 21:53:45 +0400

    {"types":"C","runs":42}

--- problem43.java
+++ problem43.java
@@ -1,4 +1,19 @@
+import java.awt.Color;
 
-public class problem43 {
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
 
+public class problem43 extends GraphicsProgram{
+	private static final int RADIUS = 30;
+    public void run(){
+    	addCircle();
+    }
+	private void addCircle() {
+		GOval circle = new GOval(2 * RADIUS, 2 * RADIUS);
+		int x = getWidth() / 2 - RADIUS;
+		int y = getHeight() / 2 - RADIUS;
+		circle.setFilled(true);
+		circle.setColor(Color.BLACK);
+		add(circle, x, y);
+	}
 }
