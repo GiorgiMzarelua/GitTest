1dd837c5f1092b401e93714ca68f3b851d43a597
S <s>
2023-11-16 12:38:52 +0400

    {"types":"C","runs":3}

--- problem46.java
+++ problem46.java
@@ -1,4 +1,23 @@
+import acm.program.ConsoleProgram;
 
-public class problem46 {
-
+public class problem46 extends ConsoleProgram{
+    public void run(){
+    	String s = readLine("Enter the text: ");
+    	char c;
+    	int n = s.length();
+    	int counter = 1;
+    	int ans = 0;
+    	for(int i = 0; i < n; i++){
+    		c = s.charAt(i);
+    	    for(int j = i; j < n; j++){
+    	    	if(c == s.charAt(j)){
+    	    		counter++;
+    	    	}
+    	    	if(counter > ans){
+    	    		ans = counter;
+    	    	}
+    	    	counter = 1;
+    	    }
+    	}
+    }
 }
