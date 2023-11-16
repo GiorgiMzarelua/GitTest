dbd8c1c7254c3f2c71fbbe071f65837a5424468c
S <s>
2023-11-16 17:40:35 +0400

    {"types":"C","runs":15}

--- semina16ri1.java
+++ semina16ri1.java
@@ -1,4 +1,9 @@
+import acm.program.ConsoleProgram;
 
-public class semina16ri1 {
-
+public class semina16ri1 extends ConsoleProgram{
+	public void run(){
+		String s1 = readLine("Enter the first string: ");
+		String s2 = readLine("Enter the second string: ");
+		
+	}
 }
