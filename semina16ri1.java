c8808d4d67ffbf8f6de46d7a3ad56f0a6e3987d2
S <s>
2023-11-16 17:50:30 +0400

    {"types":"C","runs":16}

--- semina16ri1.java
+++ semina16ri1.java
@@ -4,6 +4,32 @@ public class semina16ri1 extends ConsoleProgram{
 	public void run(){
 		String s1 = readLine("Enter the first string: ");
 		String s2 = readLine("Enter the second string: ");
-		
+		if(s1.length() != s2.length()){
+			print("FALSE");
+		}
+		else{
+			int n = s1.length();
+			int counter1 = 0;
+			int counter2 = 0;
+			for(int i = 0; i < n; i++){
+				for(int j = i + 1; j< n; j++){
+					if(s1.charAt(i) == s1.charAt(j)){
+						counter1++;
+					}
+				}
+				for(int j = 0; j < n; j++){
+					if(s1.charAt(i) == s2.charAt(j)){
+						counter2++;
+					}
+					if(counter1 != counter2){
+						print("FALSE");
+						break;
+					}
+					counter1 = 0;
+					counter2 = 0;
+				}
+			}
+			print("TRUE");
+		}
 	}
 }
