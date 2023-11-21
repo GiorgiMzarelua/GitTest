dafa12c393e17647d0d08d80c0281009a4510c69
S <s>
2023-11-21 17:42:34 +0400

    {"types":"C","runs":25}

--- problem49.java
+++ problem49.java
@@ -1,4 +1,11 @@
+import acm.program.ConsoleProgram;
 
-public class problem49 {
-
+public class problem49 extends ConsoleProgram{
+    public void run(){
+    	String s = readLine("Enter the text: ");
+    	double ans = 1;
+    	for(int i = 0; i < s.length(); i++){
+    		ans = Math.pow(10, i) * ans + (int)s.charAt(i);  
+    	}
+    }
 }
