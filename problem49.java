315119cbad611d1ce8567f0d4887bb2f0c059950
S <s>
2023-11-21 17:43:42 +0400

    {"types":"C","runs":27}

--- problem49.java
+++ problem49.java
@@ -4,7 +4,7 @@ public class problem49 extends ConsoleProgram{
     public void run(){
     	String s = readLine("Enter the text: ");
     	double ans = 1;
-    	for(int i = 0; i < s.length(); i++){
+    	for(int i = s.length() - 1; i > 0; i--){
     		ans = Math.pow(10, i) * ans + (int)s.charAt(i);  
     	}
     	print(ans);
