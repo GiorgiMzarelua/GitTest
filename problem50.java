e630bc5c34eaa4c0cfd98463f09fb09dd16fcba2
S <s>
2023-11-22 18:20:00 +0400

    {"types":"C","runs":1}

--- problem50.java
+++ problem50.java
@@ -1,7 +1,10 @@
+import java.util.StringTokenizer;
+
 import acm.program.ConsoleProgram;
 
 public class problem50 extends ConsoleProgram{
     public void run(){
-    	
+    	String s = readLine("Enter the text: ");
+    	println("There is the number of word in the entered sentence: " + equals( new StringTokenizer(s). countTokens()));
     }
 }
