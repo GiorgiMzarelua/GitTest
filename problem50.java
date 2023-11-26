184e7010c8d9be549c26dac3fd6006d15b0d9a64
S <s>
2023-11-26 18:07:43 +0400

    {"types":"C","runs":1}

--- problem50.java
+++ problem50.java
@@ -4,8 +4,8 @@ import acm.program.ConsoleProgram;
 
 public class problem50 extends ConsoleProgram{
     public void run(){
-    	String s = readLine("Enter the text: ");
-    	StringTokenizer tk = 
-    	
-    }
+    	String s = readLine("Enter the text to tokenize: ");
+    	StringTokenizer countTokens = new StringTokenizer(s);
+    	println("there are " + countTokens + " tokens");
+    }	
 }
