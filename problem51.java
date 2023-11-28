ac73094ec87d4a038dc4a3876f259304e79b3c36
S <s>
2023-11-28 17:42:00 +0400

    {"types":"C","runs":2}

--- problem51.java
+++ problem51.java
@@ -1,7 +1,21 @@
+import java.util.StringTokenizer;
+
 import acm.program.ConsoleProgram;
 
 public class problem51 extends ConsoleProgram{
     public void run(){
-    	
+    	String s = readLine("Enter the text: ");
+    	String ans = removeDuplicates(s);
+    	println(ans);
+    }
+    private String removeDuplicates(String s){
+    	String result = "";
+    	for(int i = 0; i < s.length(); i ++){
+    		char ch = s.charAt(i);
+    		if(s.indexOf(ch) == i){
+    			result += ch;
+    		}
+    	}
+		return result;
     }
 }
