785d568930e6fb02280d02d70417a37d009a6aa1
S <s>
2023-11-28 20:08:35 +0400

    {"types":"C","runs":8}

--- midTerm2.java
+++ midTerm2.java
@@ -9,7 +9,7 @@ public class midTerm2 extends ConsoleProgram{
 	private boolean isSorted(String text) {
 		for(int i = 0; i < text.length(); i++){
 			if((int)text.charAt(i) > 96 && (int)text.charAt(i) < 123){
-				for(int j = i + 1; j < text.length(); j ++){
+				for(int j = i + 1; j < text.length(); j++){
 					if((int)text.charAt(j) > 96 && (int)text.charAt(j) < 123 && (int)text.charAt(i) >= (int)text.charAt(j)){
 						return false;
 					}
