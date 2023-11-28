ff224172bf9a09edf464f9b0fe820e667f769243
S <s>
2023-11-28 18:01:51 +0400

    {"types":"C","runs":7}

--- midTerm2.java
+++ midTerm2.java
@@ -1,4 +1,21 @@
+import acm.program.ConsoleProgram;
 
-public class midTerm2 {
+public class midTerm2 extends ConsoleProgram{
+    public void run(){
+    	String text = readLine("Enter the text: ");
+    	println(isSorted(text));
+    }
 
+	private boolean isSorted(String text) {
+		for(int i = 0; i < text.length(); i++){
+			if((int)text.charAt(i) > 96 && (int)text.charAt(i) < 123){
+				for(int j = i + 1; j < text.length(); j ++){
+					if((int)text.charAt(j) > 96 && (int)text.charAt(j) < 123 && (int)text.charAt(i) >= (int)text.charAt(j)){
+						return false;
+					}
+				}
+			}
+		}
+		return true;
+	}
 }
