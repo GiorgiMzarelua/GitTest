38abab6f371526b387f9ce94db1b09e3dc21d9c3
S <s>
2023-11-30 17:12:05 +0400

    {"types":"C","runs":1}

--- seminari20.java
+++ seminari20.java
@@ -1,4 +1,21 @@
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
 
-public class seminari20 {
-
+public class seminari20 extends GraphicsProgram{
+	private GOval circle;
+	
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+    public void run(){
+    	addBall();
+    }
+	private void addBall() {
+		double x = rgen.nextDouble(50, getWidth() - 50);
+		double y = rgen.nextDouble(50, getHeight() - 50);
+		double radius = rgen.nextDouble(20, 150);
+		circle = new GOval(x, y, 2 * radius, 2 * radius);
+		circle.setFilled(true);
+		circle.setColor(rgen.nextColor());
+		add(circle);
+	}
 }
