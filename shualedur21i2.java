36ad86858ed694e40db1a46e4113b676daf9dadb
S <s>
2023-12-01 15:54:18 +0400

    {"types":"C","runs":9}

--- shualedur21i2.java
+++ shualedur21i2.java
@@ -1,4 +1,25 @@
+import acm.program.ConsoleProgram;
 
-public class shualedur21i2 {
+public class shualedur21i2 extends ConsoleProgram{
+    public void run(){
+    	String lukeDna = readLine("Enter Luke's DNA: ");
+    	String anakineDna = readLine("Enter Anakine's DNA: ");
+    	boolean isFather = amIYourFatherLuke(lukeDna, anakineDna);
+    }
 
+	private boolean amIYourFatherLuke(String lukeDna, String anakineDna) {
+		int n1 = lukeDna.length();
+		int n2 = anakineDna.length();
+		int counter = 1;
+		while(true){
+			for(int i = 0; i < n2; i++){
+				for(int j = 0; j < n1; j++){
+					if(anakineDna.charAt(i) == lukeDna.charAt(j)){
+						
+					}
+				}
+			}
+		}
+		return false;
+	}
 }
--- shualedur21i3.java
+++ shualedur21i3.java
@@ -1,4 +1,19 @@
+import java.awt.event.MouseEvent;
 
-public class shualedur21i3 {
+import acm.graphics.GLine;
+import acm.program.GraphicsProgram;
 
+public class shualedur21i3 extends GraphicsProgram{
+    public void run(){
+    	addMouseListeners();
+    }
+    @Override
+	public void mouseClicked(MouseEvent e){
+    	int x = 0;
+    	int y = 0;
+    	GLine line = new GLine(x, y, e.getX(), e.getY());
+    	x = e.getX();
+    	y = e.getY();
+    	add(line);
+    }
 }
