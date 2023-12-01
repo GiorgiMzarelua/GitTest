ebb4200f937b0d6a9c3024003669706bfb5fdd23
S <s>
2023-12-01 22:54:30 +0400

    {"types":"C","runs":28}

--- shualedur21i3.java
+++ shualedur21i3.java
@@ -1,15 +1,20 @@
 import java.awt.event.MouseEvent;
 
 import acm.graphics.GLine;
+import acm.graphics.GObject;
+import acm.graphics.GPoint;
 import acm.program.GraphicsProgram;
 
 public class shualedur21i3 extends GraphicsProgram{
+	private GObject POINT = null;
     public void run(){
     	addMouseListeners();
     	
     }
     @Override
 	public void mouseClicked(MouseEvent e){
-    	
+    	GLine line = new GLine(POINT.getX(), POINT.getY(), e.getX(), e.getY());
+    	POINT = getElementAt(e.getX(), e.getY());
+    	add(line);
     }
 }
