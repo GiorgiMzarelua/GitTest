528bc557f9578ac6a29004a9d6499ecdc2f1215f
S <s>
2023-12-01 15:37:58 +0400

    {"types":"C","runs":5}

--- shualedur21i1.java
+++ shualedur21i1.java
@@ -4,18 +4,17 @@ public class shualedur21i1 extends ConsoleProgram{
     public void run(){
     	int number1 = readInt("Enter the first number: ");
     	int number2 = readInt("Enter the second number: ");
-    	int ans = 0;
-    	for(int i = 1; i <= number2; i++){
-    		int j = i;
-    		while(j > 0){
-    			int number = 1;
-    			number = number * number1;
-    			ans++;
-    			if(number >= number2){
-    				break;
-    			}
-    		}
-    	}
-    	print(ans - 1);
+    	int ans = log(number1, number2);
+    	print(ans);
     }
+
+	private int log(int number1, int number2) {
+		int number = 1;
+		int ans = 0;
+		while(number <= number2){
+			number = number1 * number;
+			ans++;
+		}
+		return (ans - 1);
+	}
 }
