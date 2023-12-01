92a13d6beb8b8ba9ff8005ebb90c4224e99a9926
S <s>
2023-12-01 21:15:52 +0400

    {"types":"C","runs":16}

--- shualedur22i3.java
+++ shualedur22i3.java
@@ -1,4 +1,15 @@
+import acm.program.ConsoleProgram;
 
-public class shualedur22i3 {
+public class shualedur22i3 extends ConsoleProgram{
+    public void run(){
+    	String text = readLine("Enter the text: ");
+    	String textAfterManipulation = readLine("Enter the text to check: ");
+    	boolean isPossible = manipulation(text, textAfterManipulation);
+    	println(isPossible);
+    }
 
+	private boolean manipulation(String text, String textAfterManipulation) {
+		
+		return false;
+	}
 }
