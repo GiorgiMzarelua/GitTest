2577a4e8b5ec2638791b4ad0c8baf089f4c957a7
S <s>
2023-12-01 15:34:44 +0400

    {"types":"C","runs":4}

--- shualedur21i1.java
+++ shualedur21i1.java
@@ -1,4 +1,21 @@
+import acm.program.ConsoleProgram;
 
-public class shualedur21i1 {
-
+public class shualedur21i1 extends ConsoleProgram{
+    public void run(){
+    	int number1 = readInt("Enter the first number: ");
+    	int number2 = readInt("Enter the second number: ");
+    	int ans = 0;
+    	for(int i = 1; i <= number2; i++){
+    		int j = i;
+    		while(j > 0){
+    			int number = 1;
+    			number = number * number1;
+    			ans++;
+    			if(number >= number2){
+    				break;
+    			}
+    		}
+    	}
+    	print(ans - 1);
+    }
 }
