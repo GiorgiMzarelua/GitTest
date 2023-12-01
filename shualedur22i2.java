a0b4cbcb1a04f58aece0c8b2540b36d943e2a820
S <s>
2023-12-01 21:15:52 +0400

    {"types":"C","runs":16}

--- shualedur22i2.java
+++ shualedur22i2.java
@@ -1,4 +1,21 @@
+import acm.graphics.GRect;
+import acm.program.GraphicsProgram;
 
-public class shualedur22i2 {
+public class shualedur22i2 extends GraphicsProgram{
+    public void run(){
+    	drawGrid();
+    }
 
+	private void drawGrid() {
+		for(int i = 0; i < 8; i++){
+			drawALine(i);
+		}
+	}
+
+	private void drawALine(int i) {
+		for(int j = 0; j < 8; j++){
+			GRect rect = new GRect(j * getWidth() / 8, i * getWidth() / 8, getWidth() / 8, getWidth() / 8);
+            add(rect);
+		}
+	}
 }
