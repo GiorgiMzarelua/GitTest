7733912ab3e3592008b95e65976e7aea013e42c8
S <s>
2023-12-01 16:13:01 +0400

    {"types":"C","runs":11}

--- shualedur21i2.java
+++ shualedur21i2.java
@@ -5,21 +5,31 @@ public class shualedur21i2 extends ConsoleProgram{
     	String lukeDna = readLine("Enter Luke's DNA: ");
     	String anakineDna = readLine("Enter Anakine's DNA: ");
     	boolean isFather = amIYourFatherLuke(lukeDna, anakineDna);
+    	print(isFather);
     }
 
 	private boolean amIYourFatherLuke(String lukeDna, String anakineDna) {
 		int n1 = lukeDna.length();
 		int n2 = anakineDna.length();
-		int counter = 1;
+		int counter = 0;
 		while(true){
 			for(int i = 0; i < n2; i++){
 				for(int j = 0; j < n1; j++){
 					if(anakineDna.charAt(i) == lukeDna.charAt(j)){
-						
+						while(anakineDna.charAt(i + counter) == lukeDna.charAt(j + counter)){
+							counter++;	
+						}
+						if(counter > n2 / 2){
+							return true;
+						}
+						else{
+							counter = 0;
+						}
 					}
 				}
 			}
+			return false;
 		}
-		return false;
+		
 	}
 }
