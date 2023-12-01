e490567a74127045720e6521b7195df4f8fc7e90
S <s>
2023-12-01 23:30:23 +0400

    {"types":"C","runs":38}

--- shualedur21i2.java
+++ shualedur21i2.java
@@ -16,7 +16,7 @@ public class shualedur21i2 extends ConsoleProgram{
 			for(int j = i + 1; j < n; j++){
 				for(int k = 0; k < m; k++){
 					for(int f = k + 1; f < m; f++){
-						if(lukeDna.substring(i, j) == anakineDna.subSequence(k, f) && anakineDna.subSequence(k, f).length() > ans.length()){
+						if(lukeDna.substring(i, j) == anakineDna.substring(k, f) && anakineDna.substring(k, f).length() > ans.length()){
 							ans = lukeDna.substring(i, j);
 						}
 					}
