d31cfe1b6537956c798aa91e75a0d0fa3fee8b28
S <s>
2023-12-01 16:13:02 +0400

    {"types":"C","runs":11}

--- shualedur21i3.java
+++ shualedur21i3.java
@@ -6,12 +6,10 @@ import acm.program.GraphicsProgram;
 public class shualedur21i3 extends GraphicsProgram{
     public void run(){
     	addMouseListeners();
+    	
     }
     @Override
 	public void mouseClicked(MouseEvent e){
-    	int x = e.getX();
-    	int y = e.getY();
-    	GLine line = new GLine(x, y, e.getX(), e.getY());
-    	add(line);
+    	
     }
 }
