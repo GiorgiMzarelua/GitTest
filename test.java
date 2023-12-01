a0dbd3480a9fec51e4e6c83c02d0d4129bf9bffe
S <s>
2023-12-01 23:27:16 +0400

    {"types":"C","runs":35}

--- test.java
+++ test.java
@@ -3,7 +3,7 @@ import acm.program.ConsoleProgram;
 public class test extends ConsoleProgram{
     public void run(){
     	String s = readLine();
-    	String w = s.substring(5, 8);
+    	String w = s.substring(5, 5);
     	println(w);
     }
 }
