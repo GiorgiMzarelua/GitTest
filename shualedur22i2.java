f4ba5a61d8f5a1b1df3e39895f58570e1f9ff1f1
S <s>
2023-12-01 21:35:16 +0400

    {"types":"C","runs":19}

--- shualedur22i2.java
+++ shualedur22i2.java
@@ -1,8 +1,14 @@
+import java.awt.event.MouseEvent;
+
+import com.sun.prism.paint.Color;
+
+import acm.graphics.GObject;
 import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 
 public class shualedur22i2 extends GraphicsProgram{
     public void run(){
+    	addMouseListeners();
     	drawGrid();
     }
 
@@ -15,7 +21,15 @@ public class shualedur22i2 extends GraphicsProgram{
 	private void drawALine(int i) {
 		for(int j = 0; j < 8; j++){
 			GRect rect = new GRect(j * getHeight() / 8, i * getHeight() / 8, getHeight() / 8, getHeight() / 8);
+			rect.setFilled(true);
+			rect.setFillColor(Color.WHITE);
             add(rect);
+		}
+	}
+	@Override
+	public void mouseClicked(MouseEvent e){
+		if(getElementAt(e.getX(), e.getY()) != null){
+			
 		}
 	}
 }
