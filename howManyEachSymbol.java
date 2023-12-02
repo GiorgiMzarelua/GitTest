4a9b3671ad2bc7191fdf12ffaa507809fd5bb3dc
S <s>
2023-12-02 15:26:22 +0400

    {"types":"C","runs":50}

--- howManyEachSymbol.java
+++ howManyEachSymbol.java
@@ -1,4 +1,23 @@
+import acm.program.ConsoleProgram;
 
-public class howManyEachSymbol {
+public class howManyEachSymbol extends ConsoleProgram{
+	private String w;
+	
+    public void run(){
+    	String s =readLine("Enter the text: ");
+        int isBalanced = isBalanced(s);
+        println(isBalanced);
+    }
 
+	private int isBalanced(String s) {
+		int counter = 0;
+		for(int i = 0; i < s.length(); i++){
+			for(int j = i; j < s.length(); j++){
+				if(s.charAt(i) == s.charAt(j)){
+					counter++;
+				}
+			}
+		}
+		return counter;
+	}
 }
