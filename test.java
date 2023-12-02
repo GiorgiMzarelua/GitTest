1df036f7c2c4feddc0ae060653f928deba569ea3
S <s>
2023-12-02 15:18:28 +0400

    {"types":"C","runs":49}

--- test.java
+++ test.java
@@ -2,11 +2,6 @@ import acm.program.ConsoleProgram;
 
 public class test extends ConsoleProgram{
     public void run(){
-    	for(int i = 0; i < 10; i++){
-    		for(int j = 11; j < 20; j++ ){
-    			println(j);
-    			break;
-    		}
-    	}
+    	
     }
 }
