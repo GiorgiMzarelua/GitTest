59024b5c27a22bf72e5eecc018e236384a3b6d47
S <s>
2023-12-02 10:14:02 +0400

    {"types":"C","runs":39}

--- shualedur21i2.java
+++ shualedur21i2.java
@@ -4,25 +4,35 @@ public class shualedur21i2 extends ConsoleProgram{
     public void run(){
     	String lukeDna = readLine("Enter Luke's DNA: ");
     	String anakineDna = readLine("Enter Anakine's DNA: ");
-    	String isFather = amIYourFatherLuke(lukeDna, anakineDna);
+    	boolean isFather = amIYourFatherLuke(lukeDna, anakineDna);
     	print(isFather);
     }
-
-	private String amIYourFatherLuke(String lukeDna, String anakineDna) {
-		int n = lukeDna.length();
-		int m = anakineDna.length();
-		String ans = null;
-		for(int i  = 0; i < n; i++){
-			for(int j = i + 1; j < n; j++){
-				for(int k = 0; k < m; k++){
-					for(int f = k + 1; f < m; f++){
-						if(lukeDna.substring(i, j) == anakineDna.substring(k, f) && anakineDna.substring(k, f).length() > ans.length()){
-							ans = lukeDna.substring(i, j);
-						}
-					}
+    
+    private boolean amIYourFatherLuke(String lukeDna, String anakineDna){
+		int maxLength = 0;
+		for(int i = 0; i < lukeDna.length(); i++){
+			//!! to include substring at the end =>  j < lukeDNA.length() + 1
+			for(int j = i + 1; j < lukeDna.length() + 1; j++){
+				String currSubs = lukeDna.substring(i, j);
+				if(anakineDna.indexOf(currSubs) == -1){
+					break;
 				}
+				maxLength = Math.max(maxLength, currSubs.length());
 			}
 		}
-		return ans;
+		return maxLength > anakineDna.length() / 2.0;
 	}
-}	
+}    
+
+//	private String amIYourFatherLuke(String lukeDna, String anakineDna) {
+//		int n = lukeDna.length();
+//		int m = anakineDna.length();
+//		String ans = null;
+//		for(int i  = 0; i < n; i++){
+//			for(int j = i + 1; j < n + 1; j++){
+//				
+//			}
+//		}
+//		return ans;
+//	}
+//}	
