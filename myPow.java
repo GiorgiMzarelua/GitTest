96f537c2dc838f5b65415b08662b59f420bf3362
S <s>
2023-12-04 22:10:58 +0400

    {"types":"C","runs":8}

--- myPow.java
+++ myPow.java
@@ -1,4 +1,20 @@
+import acm.program.ConsoleProgram;
 
-public class myPow {
-
+public class myPow extends ConsoleProgram{
+    public void run(){
+    	int n = readInt("Enter the power: ");
+    	double x = readDouble("Enter the number: ");
+    	double ans = 1;
+        if(n >= 0){
+            for(int i = 0; i < n; i++){
+            ans = ans * x;
+            }
+        }
+        else{
+            for(int i = 0; i > n; i--){
+                ans = ans / x;
+            }
+        }
+        println(ans);
+    }
 }
