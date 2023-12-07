2802257b261366b78217c06e1362430426e4e92d
S <s>
2023-12-07 17:20:13 +0400

    {"types":"C","runs":1}

--- selectionSort.java
+++ selectionSort.java
@@ -1,4 +1,23 @@
+import acm.program.ConsoleProgram;
 
-public class selectionSort {
-
+public class selectionSort extends ConsoleProgram{
+    public void run(){
+    	int n = readInt("Enter the quantity of numbers: ");
+    	int[] array = new int[100];
+    	int minimum = 0;
+    	for(int i = 0; i < n; i++){
+    		array[i] = readInt("Enter the " + i + "-th number of the list");
+    	}
+    	minimum = array[0];
+    	for(int i = 0; i < n; i++){
+    		for(int j = i; j < n; j ++){
+    			if(array[j] < minimum){
+    				int k = array[j];
+    				array[j] = minimum;
+    				minimum = k;
+    				array[i] = minimum;
+    			}
+    		}
+    	}
+    }
 }
