41254844a4ec4b33d0cd14eefe0c07875eac0c79
S <s>
2023-12-08 14:02:03 +0400

    {"types":"C","runs":1}

--- bubleSort.java
+++ bubleSort.java
@@ -1,4 +1,22 @@
+import java.util.Arrays;
 
-public class bubleSort {
+import acm.program.ConsoleProgram;
 
+public class bubleSort extends ConsoleProgram{
+    public void run(){
+    	int n = readInt("Enter the quantity of numbers: ");
+    	int[] array = new int[n];
+    	for(int i = 0; i < n; i++){
+    		array[i] = readInt("Enter the " + i + "-th number of the list: ");
+    	}
+    	int k;
+    	for(int i = 0; i < n - 1; i++){
+    		for(int j = i; j < n - 1; j++){
+    			k = Math.min(array[j], array[j + 1]);
+    			array[j + 1] = Math.max(array[j], array[j + 1]);
+    			array[j] = k;
+    		}
+    	}
+    	println(Arrays.toString(array));
+    }
 }
