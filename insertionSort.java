481e8c18d15f0bdfb0925adcdb4813a787f1bc05
S <s>
2023-12-08 16:26:23 +0400

    {"types":"C","runs":17}

--- insertionSort.java
+++ insertionSort.java
@@ -9,20 +9,20 @@ public class insertionSort extends ConsoleProgram{
         for(int i = 0; i < n; i++){
         	array[i] = readInt("Enter the " + (i + 1) + "-th number of the list: ");
         }
-        for(int i = 0; i < n - 1; i++){
+        for(int i = 0; i < n; i++){
         	int key = array[i];
         	for(int j = i; j < n; j++){
         		if(array[j] < key){
-        			swap(key, array[j]);
+        			swap(array, key, j);
         		}
         	}
         }
         println(Arrays.toString(array));
     }
 
-	private void swap(int key, int i) {
-		int k = key;
-		key = i;
-		i = k;
+	private void swap(int[] array, int key, int j) {
+		int m = key;
+		key = array[j];
+		array[j] = m;
 	}
 }
