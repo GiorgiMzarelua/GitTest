ee9de608d5208eb71549ed736c59c526f6caa1fe
S <s>
2023-12-08 14:04:57 +0400

    {"types":"C","runs":8}

--- bubleSort.java
+++ bubleSort.java
@@ -10,7 +10,7 @@ public class bubleSort extends ConsoleProgram{
     		array[i] = readInt("Enter the " + i + "-th number of the list: ");
     	}
     	int k;
-    	for(int i = 0; i < n - 1; i++){
+    	for(int i = 0; i < n; i++){
     		for(int j = i; j < n - 1; j++){
     			k = Math.min(array[j], array[j + 1]);
     			array[j + 1] = Math.max(array[j], array[j + 1]);
