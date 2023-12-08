22b114ee937e4c3621bb155c385900d04379be64
S <s>
2023-12-08 14:26:02 +0400

    {"types":"C","runs":12}

--- insertionSort.java
+++ insertionSort.java
@@ -1,4 +1,21 @@
+import java.util.Arrays;
 
-public class insertionSort {
+import acm.program.ConsoleProgram;
 
+public class insertionSort extends ConsoleProgram{
+    public void run(){
+        int n = readInt("Enter the quantity of numbers: ");
+        int[] array = new int[n];
+        for(int i = 0; i < n; i++){
+        	array[i] = readInt("Enter the " + (i + 1) + "-th number of the list: ");
+        }
+        int key1 = array[0];
+        int key2 = array[0];
+        for(int i = 0; i < n - 1; i++){
+        		key1 = Math.min(key2, array[i]);
+        		key2 = Math.max(key2, array[i]);
+        		array[i] = key1;                
+        }
+        println(Arrays.toString(array));
+    }
 }
