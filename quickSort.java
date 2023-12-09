33013b6461a14d727924c6e1498e7d6d4397f4f5
S <s>
2023-12-10 01:48:04 +0400

    {"types":"C","runs":21}

--- quickSort.java
+++ quickSort.java
@@ -2,6 +2,16 @@ import acm.program.ConsoleProgram;
 
 public class quickSort extends ConsoleProgram{
     public void run(){
-    	
+    	int n = readInt("Enter the quantity of numbers: ");
+    	int[] array = new int[n];
+    	for(int i = 0; i < n; i++){
+    		array[i] = readInt("Enter the " + (i + 1) + "-th number of the list: ");
+    	}
+    	int pointer = array[n-1];
+    	int greater;
+    	int smaller;
+    	for(int i = 0; i < n; i++){
+    		
+    	}
     }
 }
