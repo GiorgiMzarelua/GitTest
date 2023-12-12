4dfc20d8a8a6b0135dc9df9f1c6162e558d3279a
S <s>
2023-12-12 17:40:20 +0400

    {"types":"C","runs":17}

--- problem60.java
+++ problem60.java
@@ -1,4 +1,21 @@
+import java.util.ArrayList;
 
-public class problem60 {
+import acm.program.ConsoleProgram;
 
+public class problem60 extends ConsoleProgram{
+    public void run(){
+    	 ArrayList <Integer> intList = new ArrayList <Integer>();
+    	 int counter = 0;
+    	 while(true){
+    		 int n = readInt("Enter the number: ");
+    		 if(n == -1){
+    			 break;
+    		 }
+    	     intList.add(n);
+    	 }
+    	 for(int i  = intList.size() - 1; i >= 0; i--){
+    		 int n = intList.get(i);
+    		 println("this is the " + (i + 1) + "-th element of this arraylist: " + n);
+    	 }
+    }    
 }
