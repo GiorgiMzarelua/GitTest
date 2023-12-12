16d3adc88ef7d1cdd02190215b669a4345fe277a
S <s>
2023-12-12 17:53:43 +0400

    {"types":"C","runs":19}

--- problem57.java
+++ problem57.java
@@ -1,4 +1,32 @@
+import acm.program.ConsoleProgram;
 
-public class problem57 {
+public class problem57 extends ConsoleProgram{
+    public void run(){
+    	String s1 = readLine("Enter the first String: ");
+    	String s2 = readLine("Enter the second String: ");
+    	if(s1.length() != s2.length()){
+    		println(false);
+    	}
+    	else{
+    		boolean isAnagram = isAnagram(s1, s2);
+    		println(isAnagram);
+    	}
+    }
 
-}
+	private boolean isAnagram(String s1, String s2) {
+		int n = s1.length();
+		int[] array = new int[n];
+		for(int i = 0; i < n; i++){
+			array[(int) s1.charAt(i) - 97]++;
+		}
+		for(int i = 0; i < n; i++){
+			array[(int) s2.charAt(i) - 97]--;
+		}
+		for(int i = 0; i < n; i++){
+			if(array[i] != 0){
+				return false;
+			}
+		}
+		return true;
+	}
+} 
