ed136e705c94d7a804050def1cdf1bf7917f8b48
S <s>
2023-12-14 02:16:11 +0400

    {"types":"C","runs":2}

--- problem58.java
+++ problem58.java
@@ -7,7 +7,7 @@ public class problem58 extends ConsoleProgram{
     	int[] array = new int[n * n];
     	for(int i = 0; i < n; i++){
     		for(int j = 0; j < n; j++){
-    			matrix[i][j] = readInt("Enter the number");
+    			matrix[i][j] = readInt("Enter the number: ");
     			array[i * n + j] = matrix[i][j];
     		}
     	}
