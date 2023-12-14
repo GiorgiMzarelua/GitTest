f1dd0dba844a37332008a3b510b199ce87791274
S <s>
2023-12-14 17:03:36 +0400

    {"types":"C","runs":3}

--- problem58.java
+++ problem58.java
@@ -13,14 +13,7 @@ public class problem58 extends ConsoleProgram{
     			array[i * n + j] = matrix[i][j];
     		}
     	}
-    	for(int i = 0; i < n; i++){
-        	for(int j = i; j < n; j++){
-        		if(array[j] < array[i]){
-        			swap(array, j, i);
-        		}
-        	}
-        }
-    	println(Arrays.toString(array));
+    	
     	for(int i = 1; i <= n * n; i++){
     		if(array[i] != i){
     			println(false);
@@ -33,9 +26,5 @@ public class problem58 extends ConsoleProgram{
     	}
     }
 
-    private void swap(int[] array, int j, int i) {
-		int m = array[i];
-		array[i] = array[j];
-		array[j] = m;
-	}
+    
 }
