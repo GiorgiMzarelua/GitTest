220b60f7de38deb69fc804e5341955e672f18230
S <s>
2023-12-15 14:52:06 +0400

    {"types":"C","runs":1}

--- problem59.java
+++ problem59.java
@@ -1,4 +1,30 @@
+import acm.program.ConsoleProgram;
 
-public class problem59 {
-
-}
+public class problem59 extends ConsoleProgram{
+    public void run(){
+    	int n = readInt("Enter the length of matrix: ");
+    	int m = readInt("Enter the width of matrix: ");
+    	int[][] matrix = new int[n][m]; 
+    	for(int i = 0; i < n; i++){
+    		for(int j = 0; j < m; j++){
+    			matrix[i][j]= readInt("Enter the number: ");
+    		}
+    	}
+    	int ans = 0;
+    	int counter = 0;
+    	for(int i = 0; i < n; i++){
+    		for(int j = 0; j < m; j++){
+    			for(int k = i ; k < n; k++){
+    				for(int p = m; p < m; p++){
+    					if(matrix[k][p] == 1){
+    						counter++;
+    					}
+    				}
+    			}
+    			ans = Math.max(ans,  counter);
+    			counter = 0;
+    		}
+    	}
+    	println(ans);
+    }
+} 
