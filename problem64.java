fcef88a32da8a0bc00c290e523edbd1a8998e606
S <s>
2024-01-03 19:42:51 +0400

    {"types":"C","runs":23}

--- problem64.java
+++ problem64.java
@@ -1,4 +1,27 @@
+/*
+ * 63-ე ამოცანის გაგრძელება, დაბეჭდეთ თითოეული შეყვანილი სახელი და მის გასწვრივ
+ * მძიმით გამოყოფილი მეგობრების სია.
+ */
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
-public class problem64 {
+import acm.program.ConsoleProgram;
 
+public class problem64 extends ConsoleProgram{
+	private static final String SENTINEL = "";
+    public void run(){
+    	Map<String, ArrayList<String>> relationships = new HashMap<String, ArrayList<String>>();
+    	while(true){
+    		String S1 = readLine("Enter the person who knows the second person: ");
+    		if(S1.equals(SENTINEL)) break;
+    		String S2 = readLine("Enter the person who is known for the first one: ");
+    		if(!relationships.containsKey(S1)){
+    			relationships.put(S1, new ArrayList<String>());
+    		}
+    		ArrayList<String> knownPeople = relationships.get(S1);
+    		knownPeople.add(S2);
+    	}
+    	println(relationships);
+    }
 }
