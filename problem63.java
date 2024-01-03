06c9bda2a1696447e10891c1ca1d540fbbb175cd
S <s>
2024-01-03 17:04:43 +0400

    {"types":"C","runs":17}

--- problem63.java
+++ problem63.java
@@ -9,15 +9,14 @@ public class problem63 extends ConsoleProgram{
     public void run(){
     	Map<String, ArrayList<String>> relationships = new HashMap<String, ArrayList<String>>();
     	while(true){
-    		ArrayList<String> knownPeople = null;
     		String S1 = readLine("Enter the person who knows the second person: ");
     		if(S1.equals(SENTINEL)) break;
     		String S2 = readLine("Enter the person who is known for the first one: ");
     		if(!relationships.containsKey(S1)){
     			relationships.put(S1, new ArrayList<String>());
     		}
+    		ArrayList<String> knownPeople = relationships.get(S1);
     		knownPeople.add(S2);
-			relationships.put(S1, knownPeople);
     	}
     	println(relationships);
     }
