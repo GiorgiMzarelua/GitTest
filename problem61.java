54a3c874f49e2bb3ff19e67d83b6be9f463768cd
S <s>
2024-01-03 16:32:28 +0400

    {"types":"C","runs":6}

--- problem61.java
+++ problem61.java
@@ -1,3 +1,7 @@
+/*
+ * კონსოლიდან შეგვყავს რიცხვი m და n, შემდეგ n ცალი რიცხვი. პროგრამამ უნდა იპოვოს
+ * შეყვანილი რიცხვებიდან წყვილი რომელთა ჯამი არის m.
+ */
 import java.util.HashMap;
 import java.util.Map;
 
@@ -5,26 +9,20 @@ import acm.program.ConsoleProgram;
 
 public class problem61 extends ConsoleProgram{
     public void run(){
-    	int m = readInt("Enter the main number: ");
-    	int n = readInt("Enter the quantity of random numbers: ");
+    	int m = readInt("Enter the number to compare: ");
+    	int n = readInt("Enter the quantity of numbers: ");
     	int[] array = new int[n];
+    	Map<Integer, Integer> findingCouple = new HashMap<Integer, Integer>();
     	for(int i = 0; i < n; i++){
-    		array[i] = readInt("Enter a number: ");
+    		array[i] = readInt("Enter the " + (i + 1) + "-th number: ");
     	}
-    	Map <Integer, Integer> mp = new HashMap <Integer, Integer>();
-    	int counter = 0;
-    	for(int i = 0; i < n - 1; i++){
+    	for(int i = 0; i < n; i++){
     		for(int j = i + 1; j < n; j++){
     			if(array[i] + array[j] == m){
-    				counter++;
-    				mp.put(array[i], counter);
-    				println("The " + counter + "-th number is: " + mp.get(counter));
-    				counter++;
-    				mp.put(array[j], counter);
-    				println("The " + counter + "-th  number is: " + mp.get(counter));
+    				findingCouple.put(array[i], array[j]);
     			}
     		}
     	}
-    	
+    	println(findingCouple);
     }
 }
