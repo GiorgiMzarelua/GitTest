e18b5c2a697c266fd96bfbba4a75f93d050187ef
S <s>
2024-01-03 19:31:53 +0400

    {"types":"C","runs":18}

--- problem63.java
+++ problem63.java
@@ -1,3 +1,9 @@
+/*
+ * კონსოლიდან შეგვყავს წყვილ-წყვილად მეგობრების სახელები, მანამ სანამ არ შევიყვანთ
+ * ცარიელ სტრინგს. დაბეჭდეთ იმ ადამიანის სახელი ვისაც ჰყავს ყველაზე მეტი მეგობარი.
+ * მეგობრობა შეიძლება იყოს ცალმხრივი. თუკი შევიყვანთ A-ს და B-ს, ეს ნიშნავს, რომ A არის
+ * B-ს მეგობარი, მაგრამ B შეიძლება არ იყოს A-ს მეგობარი.
+ */
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -7,16 +13,16 @@ import acm.program.ConsoleProgram;
 public class problem63 extends ConsoleProgram{
 	private static final String SENTINEL = "";
     public void run(){
-    	Map<String, ArrayList<String>> relationships = new HashMap<String, ArrayList<String>>();
+    	Map<String, Integer> relationships = new HashMap<String,Integer>();
     	while(true){
     		String S1 = readLine("Enter the person who knows the second person: ");
     		if(S1.equals(SENTINEL)) break;
     		String S2 = readLine("Enter the person who is known for the first one: ");
     		if(!relationships.containsKey(S1)){
-    			relationships.put(S1, new ArrayList<String>());
+    			relationships.put(S1, 0);
     		}
-    		ArrayList<String> knownPeople = relationships.get(S1);
-    		knownPeople.add(S2);
+    		int newCount = relationships.get(S1) + 1;
+			relationships.put(S1, newCount);
     	}
     	println(relationships);
     }
