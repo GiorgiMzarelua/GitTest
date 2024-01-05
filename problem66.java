fa58460e275a075d326dcf4578cb11bf1a456447
S <s>
2024-01-06 01:40:19 +0400

    {"types":"C","runs":48}

--- problem66.java
+++ problem66.java
@@ -1,4 +1,24 @@
 
-public class problem66 {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
+import acm.program.ConsoleProgram;
+
+public class problem66 extends ConsoleProgram{
+	private static final String SENTINEL = "";
+    public void run(){
+    	Map<String, ArrayList<String>> mutualFriend = new HashMap<String, ArrayList<String>>();
+        while(true){
+        	String person = readLine("Enter the person who knows the second person: ");
+        	if(person.equals(SENTINEL)) break;
+        	String friend = readLine("Enter the person who is known for the first one: ");
+        	if(!mutualFriend.containsKey(person)){
+    			mutualFriend.put(person, new ArrayList<String>());
+    		}
+    		ArrayList<String> knownPeople = mutualFriend.get(person);
+    		knownPeople.add(friend);
+        }
+        
+    }
 }
