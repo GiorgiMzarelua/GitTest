0454587b65a53f8bbd93edb5936836666fb4efb6
S <s>
2024-01-06 18:55:32 +0400

    {"types":"C","runs":35}

--- buttons.java
+++ buttons.java
@@ -8,8 +8,11 @@ import acm.program.ConsoleProgram;
 public class buttons extends ConsoleProgram{
 	private static final String HELLO = "Hi";
 	private static final String HELLO_THERE = "Hello there";
+	private JTextField textField;
     public void init(){
-    	JTextField textField = new JTextField(20);
+    	textField = new JTextField(20);
+    	textField.setActionCommand(HELLO);
+    	textField.addActionListener(this);
     	add(textField, NORTH);
     	JButton but = new JButton(HELLO);
     	add(but, SOUTH);
@@ -21,5 +24,6 @@ public class buttons extends ConsoleProgram{
     	if(st.equals(HELLO)){
     		println(HELLO_THERE);
     	}
+    	
     }
 }
