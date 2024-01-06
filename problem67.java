187db69347022e47d5eea360e7e8b287898bc13c
S <s>
2024-01-06 16:52:05 +0400

    {"types":"C","runs":13}

--- problem67.java
+++ problem67.java
@@ -10,8 +10,27 @@ public class problem67 extends GraphicsProgram{
 	private JTextField textField;
 	 public void init(){
 		 addActionListeners();
-		 textField = new JTextField();
+		 textField = new JTextField(20);
 		 add(textField, SOUTH);
 		 textField.addActionListener(this);
+		 GImage image = new GImage("bad path");
+		 double x = getWidth() / 2 - image.getWidth() / 2;
+		 double y = getHeight() / 2 - image.getHeight() / 2;
+		 add(image, x, y);
+	 }
+	 @Override
+	 public void actionPerformed(ActionEvent e){
+		 if(e.getSource() == textField){
+			 try{
+				 GImage image = new GImage(textField.getText());
+					double x = getWidth()/ 2 - image.getWidth() / 2;
+					double y = getHeight()/ 2 - image.getHeight() / 2;
+					add(image, x, y);
+			 }catch(Exception ex){
+				 removeAll();
+					add(new GLabel("Error occured"), 20, 20);
+			 }
+			 textField.setText("");
+		 }
 	 }
 }
