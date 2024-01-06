618031f4e305fcfe2d302b3de38271068c45fca0
S <s>
2024-01-06 17:00:49 +0400

    {"types":"C","runs":15}

--- buttons.java
+++ buttons.java
@@ -1,10 +1,22 @@
+import java.awt.event.ActionEvent;
+
 import javax.swing.JButton;
 
 import acm.program.ConsoleProgram;
 
 public class buttons extends ConsoleProgram{
+	private static final String HELLO = "Hi";
+	private static final String HELLO_THERE = "Hello there";
     public void init(){
-    	JButton but = new JButton("Hi");
+    	JButton but = new JButton(HELLO);
     	add(but, SOUTH);
+    	addActionListeners();
+    }
+    @Override
+    public void actionPerformed(ActionEvent e){
+    	String st = e.getActionCommand();
+    	if(st.equals(HELLO)){
+    		println(HELLO_THERE);
+    	}
     }
 }
