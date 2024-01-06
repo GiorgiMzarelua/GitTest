4dd90aed8d6f727973939d99c66581d3d9407eb4
S <s>
2024-01-06 16:46:13 +0400

    {"types":"C","runs":10}

--- problem67.java
+++ problem67.java
@@ -8,30 +8,8 @@ import acm.program.GraphicsProgram;
 
 public class problem67 extends GraphicsProgram{
 	private JTextField textField;
-	
-	public void init(){
-		textField = new JTextField("20");
-		add(textField, SOUTH);
-		textField.addActionListener(this);
-		addActionListeners();
-		GImage image = new GImage("bat");
-		double x = getWidth()/ 2 - image.getWidth() / 2;
-		double y = getHeight()/ 2 - image.getHeight() / 2;
-		add(image, x, y);
-	}
-    @Override
-    public void actionPerformed(ActionEvent e){
-    	if(e.getSource() == textField){
-    		try{
-    			GImage image = new GImage(textField.getText());
-        		double x = getWidth()/ 2 - image.getWidth() / 2;
-    			double y = getHeight()/ 2 - image.getHeight() / 2;
-    			add(image, x, y);
-    		}catch(Exception ex){
-    			removeAll();
-				add(new GLabel("Error occured"), 20, 20);
-    		}
-    		textField.setText("");
-    	}
-    }
+	 public void init(){
+		 textField = new JTextField();
+		 add(textField, SOUTH);
+	 }
 }
