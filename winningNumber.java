c00dc8a2fdb190476e61a198ddddbeb638d707cf
S <s>
2024-01-07 11:50:46 +0400

    {"types":"C","runs":46}

--- winningNumber.java
+++ winningNumber.java
@@ -1,12 +1,13 @@
+import javax.swing.JFrame;
 import javax.swing.JTextField;
 
 import acm.program.ConsoleProgram;
 
 public class winningNumber extends ConsoleProgram{
 	private static final String SENTINEL = "";
-	private JTextField textField;
+	private JFrame textField;
 	public void init(){
-		textField = new JTextField(10);
+		textField = new JFrame();
 		add(textField, SOUTH);
 	}
     public void run(){
