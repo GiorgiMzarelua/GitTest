440606bdbc94786edbf2e23ac4b659efacbdb061
S <s>
2024-01-07 11:45:55 +0400

    {"types":"C","runs":42}

--- winningNumber.java
+++ winningNumber.java
@@ -1,4 +1,17 @@
+import javax.swing.JTextField;
 
-public class winningNumber {
+import acm.program.ConsoleProgram;
 
+public class winningNumber extends ConsoleProgram{
+	private static final String SENTINEL = "";
+	private JTextField textField;
+	public void init(){
+		textField = new JTextField(20);
+		add(textField, CENTER);
+	}
+    public void run(){
+    	while(true){
+    		break;
+    	}
+    }
 }
