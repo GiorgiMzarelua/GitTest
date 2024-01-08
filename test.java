768dc660ba9534902edb0a1c91681f159ca2ba07
S <s>
2024-01-08 13:40:32 +0400

    {"types":"C","runs":57}

--- test.java
+++ test.java
@@ -2,11 +2,9 @@ import acm.program.ConsoleProgram;
 
 public class test extends ConsoleProgram{
     public void run(){
-    	
+    	String s = "abcd";
+    	String w = s.substring(1,1);
+    	println(w);
     }
 }
-/*
- *   extraversion
- *   openness
- *   Conscientiousness:
- */
+
