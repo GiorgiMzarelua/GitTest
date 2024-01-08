34f9248d15bc643a812c0a977c83cc7f262fe1f8
S <s>
2024-01-08 13:45:38 +0400

    {"types":"C","runs":59}

--- longestSubstringWithoutRepeatingCharecters.java
+++ longestSubstringWithoutRepeatingCharecters.java
@@ -14,13 +14,14 @@ public class longestSubstringWithoutRepeatingCharecters extends ConsoleProgram{
         int sequence = 0;
         int counter = 1;
         while(counter < n){
-        	for(int i = 0; i <= n - counter; i++){
-        		String w = s.substring(i, i + );
+        	for(int i = 0; i < n - counter; i++){
+        		String w = s.substring(i, i + counter);
         		boolean p = isRepeating(w);
-        		if(p == true) array[sequence] = j - i + 1;
+        		if(p == true) array[sequence] = counter;
         		else array[sequence] = 1;
         		sequence++;
-        	}
+         	}
+        	counter++;
         }
         int max = 0;
         for(int i = 0; i <= sequence; i++){
