7c86c9396dd3c9711d1634c84e673cc06d9cd400
S <s>
2024-01-08 13:40:42 +0400

    {"types":"C","runs":58}

--- test.java
+++ test.java
@@ -3,7 +3,7 @@ import acm.program.ConsoleProgram;
 public class test extends ConsoleProgram{
     public void run(){
     	String s = "abcd";
-    	String w = s.substring(1,1);
+    	String w = s.substring(1,2);
     	println(w);
     }
 }
