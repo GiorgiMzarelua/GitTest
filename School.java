43fd08f51c528d783a130e02e72b2e1c1333ca2f
S <s>
2024-01-09 17:36:46 +0400

    {"types":"C","runs":72}

--- School.java
+++ School.java
@@ -1,4 +1,25 @@
+import java.util.Iterator;
 
-public class School {
-
+public class School{
+    public School(){
+    	
+    }
+    public void addTeacher(String teacher){
+    	
+    }
+    public void addSubject(String teacher, String subject){
+    	
+    }
+    public void addPupil(String pupil, String subject){
+    	
+    }
+    public Iterator<String> getTeachers(String pupil){
+    	return null;
+    }
+    public Iterator<String> getPupils(String teacher){
+		return null;
+    }
+    public void removeTeacher(String teacher){
+    	
+    }
 }
--- programmingMethodologiesClassTest.java
+++ programmingMethodologiesClassTest.java
@@ -1,4 +1,14 @@
+import java.util.ArrayList;
 
-public class programmingMethodologiesClassTest {
+import acm.program.ConsoleProgram;
 
+public class programmingMethodologiesClassTest extends ConsoleProgram{
+    public void run() {
+    	ArrayList<String> students = new ArrayList<>();
+    	students.add("gmzar23");
+    	students.add("mberi15");
+    	programmingMethodologiesClass classB = new programmingMethodologiesClass(students);
+    	println(classB.getScore("gmzar23"));
+    	println(classB.getScore("gmzar12"));
+    }
 }
