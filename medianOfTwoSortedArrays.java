d2fe9026ae220cca8322f1e5fe79a4d179475f8f
S <s>
2024-01-09 15:01:40 +0400

    {"types":"C","runs":62}

--- medianOfTwoSortedArrays.java
+++ medianOfTwoSortedArrays.java
@@ -7,10 +7,10 @@ public class medianOfTwoSortedArrays extends ConsoleProgram{
     	int[] array1 = new int[size1];
     	int[] array2 = new int[size2];
     	for(int i = 0; i < size1; i++){
-    		array1[i] = readInt("Enter the " + (i + 1) +"-th number: ");
+    		array1[i] = readInt("Enter the " + (i + 1) + "-th number of the first array: ");
     	}
     	for(int i = 0; i < size2; i++){
-    		array2[i] = readInt("Enter the " + (i + 1) + "-th number: ");
+    		array2[i] = readInt("Enter the " + (i + 1) + "-th number of the second array: ");
     	}
     	int median = medianOfTwoSortedArrays(array1, array2);
     	println("This is the median of this two merged sorted arrays: " + median);
