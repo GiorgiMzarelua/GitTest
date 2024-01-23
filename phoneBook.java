afa2e51343cc970c9c12c26171486d36564da89f
S <s>
2024-01-23 20:19:04 +0400

    {"types":"C","runs":51}

--- phoneBook.java
+++ phoneBook.java
@@ -1,4 +1,33 @@
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
-public class phoneBook {
+import acm.program.GraphicsProgram;
 
+public class phoneBook extends GraphicsProgram{
+	private JTextField name;
+	private JTextField phone;
+	private JButton add;
+	public void init(){
+		createButtons();
+		addActionListeners();
+	}
+	
+    private void createButtons() {
+		add(new JLabel("name: "), SOUTH);
+		name = new JTextField(15);
+		name.setActionCommand("name: ");
+	    name.addActionListener(this);
+		add(name,SOUTH);
+		add(new JLabel("phone: "), SOUTH);
+		phone = new JTextField(15);
+	    phone.setActionCommand("phone: ");
+	    phone.addActionListener(this);
+		add(phone, SOUTH);
+		add = new JButton("Add");
+		add.getActionCommand();
+		add.addActionListener(this);
+		add(add, SOUTH);
+	}
+	
 }
