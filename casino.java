4eea3063441b24afa7a12886513fc20c481d4206
S <s>
2024-01-23 16:29:30 +0400

    {"types":"C","runs":20}

--- casino.java
+++ casino.java
@@ -19,27 +19,57 @@ public class casino extends ConsoleProgram{
 		double lostMoney = 0;
 		double currMoney = n;
 		double betMoney = 0;
+		if(currMoney != n - lostMoney){
+			println("error");
+		}
 		while(currMoney > 0){
 			int yourNum = rgen.nextInt(0, 36);
 			int Num = rgen.nextInt(0, 36);
+			if(currMoney != n - lostMoney){
+				println("error");
+				break;
+			}
 			if(currMoney >= n){
                 betMoney = 1;				
+			}
+			if(currMoney != n - lostMoney){
+				println("error");
+				break;
 			}
 			else if(currMoney < n && currMoney >= (lostMoney + 1) / 36){
 				betMoney = (lostMoney + 1) / 36;
 			}
+			if(currMoney != n - lostMoney){
+				println("error");
+				break;
+			}
 			else if(currMoney < n && currMoney < (lostMoney + 1) / 36){
 				betMoney = currMoney;
+			}
+			if(currMoney != n - lostMoney){
+				println("error");
+				break;
 			}
 			if(yourNum == Num){
 				currMoney += betMoney * 36;
 				lostMoney -= betMoney * 36;
 			}
+			if(currMoney != n - lostMoney){
+				println("error");
+				break;
+			}
 			else{
 				currMoney -= betMoney;
 				lostMoney += betMoney;
 			}
+			if(currMoney != n - lostMoney){
+				println("error");
+				break;
+			}
 			rolls++;
+		}
+		if(currMoney != n - lostMoney){
+			println("error");
 		}
 		return rolls;
 	}
