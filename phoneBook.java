05b04fa86d596d86a58c2f16f447dcf864e443ed
S <s>
2024-01-23 20:29:46 +0400

    {"types":"C","runs":52}

--- phoneBook.java
+++ phoneBook.java
@@ -1,3 +1,5 @@
+import java.awt.event.ActionEvent;
+
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
@@ -5,6 +7,10 @@ import javax.swing.JTextField;
 import acm.program.GraphicsProgram;
 
 public class phoneBook extends GraphicsProgram{
+	private static final int MARGIN_X = 10;
+	private static final int MARGIN_Y = 10; 
+	private JLabel lastName;
+	private JLabel lastNumber;
 	private JTextField name;
 	private JTextField phone;
 	private JButton add;
@@ -28,6 +34,19 @@ public class phoneBook extends GraphicsProgram{
 		add.getActionCommand();
 		add.addActionListener(this);
 		add(add, SOUTH);
+		lastName.setLocation(0, 0);
+		lastNumber.setLocation(0, 0);
 	}
-	
+    
+    public void actionPerformed(ActionEvent e){
+    	String cmd = e.getActionCommand();
+    	if(cmd.equals("name: ")){
+    		lastName = new JLabel(name.getText() + ": ");
+    		add(lastName, MARGIN_X, lastName.getY() + MARGIN_Y);
+    	}
+    	if(cmd.equals("phone: ")){
+    		lastNumber = new JLabel(phone.getText());
+    		add(lastNumber, lastName.getX() + MARGIN_X, lastName.getY() + MARGIN_Y);
+    	}
+    }
 }
