f1042666fdb39b30c02d3ab669ef2bc4382a7149
S <s>
2024-01-23 22:30:37 +0400

    {"types":"C","runs":63}

--- similarStrings.java
+++ similarStrings.java
@@ -1,4 +1,26 @@
+import acm.program.ConsoleProgram;
 
-public class similarStrings {
+public class similarStrings extends ConsoleProgram{
+    public void run(){
+    	String S = readLine("Enter the first String: ");
+    	String T = readLine("Enter the second String: ");
+    	boolean ans = same(S, T);
+    	println(ans);
+    }
 
+	private boolean same(String S, String T) {
+		int n = S.length();
+		for(int i = 0; i < n; i++){
+			if(S.charAt(i) != T.charAt(i) && S.charAt(n - i - 1) != T.charAt(n - i - 1)){
+				return false;
+			}
+			else if(S.charAt(i) != T.charAt(n - i - 1) && S.charAt(n - i - 1) != T.charAt(i)){
+				return false;
+			}
+			else if(S.charAt(i) != S.charAt(n - i - 1) && T.charAt(i) != T.charAt(n - i - 1)){
+				return false;
+			}
+		}
+		return true;
+	}
 }
