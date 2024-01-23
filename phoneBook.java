c31e85483b04e766ace2ab262907de86008010df
S <s>
2024-01-23 21:10:29 +0400

    {"types":"C","runs":59}

--- phoneBook.java
+++ phoneBook.java
@@ -9,7 +9,6 @@ import acm.program.GraphicsProgram;
 public class phoneBook extends GraphicsProgram{
 	private static final int MARGIN_X = 10;
 	private static final int MARGIN_Y = 10; 
-	private int x = 0;
 	private int y = 0;
 	private JLabel label;
 	private JTextField name;
@@ -23,32 +22,26 @@ public class phoneBook extends GraphicsProgram{
     private void createButtons() {
 		add(new JLabel("name: "), SOUTH);
 		name = new JTextField(15);
-		name.setActionCommand("name: ");
-	    name.addActionListener(this);
+		name.setActionCommand("Add");
 		add(name,SOUTH);
 		add(new JLabel("phone: "), SOUTH);
 		phone = new JTextField(15);
-	    phone.setActionCommand("phone: ");
-	    phone.addActionListener(this);
+	    phone.setActionCommand("Add");
 		add(phone, SOUTH);
 		add = new JButton("Add");
 		add.getActionCommand();
 		add.addActionListener(this);
 		add(add, SOUTH);
-		
 	}
     
     public void actionPerformed(ActionEvent e){
         String cmd = e.getActionCommand();
-        if(cmd.equals("name: ")){
-        	label = new JLabel(name.getText());
+        if(cmd.equals("Add")){
+        	String s1 = name.getText();
+        	String s2 = phone.getText();
+        	label = new JLabel(s1 + " " + s2);
         	add(label, MARGIN_X, y + MARGIN_Y);
-        	x = MARGIN_X + 5 * label.getText().length() + MARGIN_X;
-        }
-        else if(cmd.equals("phone: ")){
-        	label = new JLabel(phone.getText());
-        	add(label, x + MARGIN_X, y + MARGIN_Y);
-        	y = label.getY() + 2 * MARGIN_Y;
+        	y = label.getY();
         }
     }
 }
