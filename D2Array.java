9bb3f6feb39b79ef1b4be3ce88a5e7ccab854f46
S <s>
2024-01-23 22:54:38 +0400

    {"types":"C","runs":75}

--- D2Array.java
+++ D2Array.java
@@ -1,6 +1,7 @@
 import acm.program.ConsoleProgram;
 
 public class D2Array extends ConsoleProgram{
+	private int[][] matrix2;
     public void run(){
     	int n = readInt("Enter the quantity of numbers: ");
     	int[] array = new int[n];
@@ -12,13 +13,12 @@ public class D2Array extends ConsoleProgram{
     }
 
 	private int[][] create(int[] array) {
-		int[][] matrix = new int[array.length][];
 		for(int i = 0; i < array.length; i++){
 			for(int j = 0; j < array[i]; j++){
-				matrix[i][j] = 0;
+				matrix2[i][j] = 0;
 			}
 		}
-		return matrix;
+		return matrix2;
 	}
 
 	
