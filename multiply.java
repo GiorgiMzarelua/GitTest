c9052bfaa3df799dda0ce6cbb2fe9614f10ddb76
S <s>
2024-01-24 15:12:31 +0400

    {"types":"C","runs":97}

--- multiply.java
+++ multiply.java
@@ -31,7 +31,17 @@ public class multiply extends ConsoleProgram{
     }
 
 	private ArrayList<Integer> multiply(ArrayList<Integer> num1, ArrayList<Integer> num2) {
-		
+		ArrayList<Integer> list1 = new ArrayList<>();
+		ArrayList<Integer> list2 = new ArrayList<>();
+		int rem = 0;
+		for(int i = num1.size() - 1; i >= 0; i--){
+			for(int j = num2.size() - 1; j >= 0; j--){
+				list1.add(num1.get(i) * num2.get(j) % 10 + rem);
+				rem = num1.get(i) * num2.get(j) / 10;
+				
+			}
+			list1.add(num1.get(num1.size() - 1) * num2.get(num2.size() - 1) / 10 );
+		}
 		return null;
 	}
 }
