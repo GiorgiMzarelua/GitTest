9e14ac5755d3e3055aeaa49583668e6a48dd4aff
S <s>
2024-01-24 20:15:39 +0400

    {"types":"C","runs":102}

--- undoRedoTest.java
+++ undoRedoTest.java
@@ -4,11 +4,6 @@ import acm.program.ConsoleProgram;
 
 public class undoRedoTest extends ConsoleProgram{
     public void run(){
-    	ArrayList<String> list = new ArrayList<String>();
-    	undoRedo classB = new undoRedo();
-    	classB.save("1");
-    	classB.save("2");
-    	classB.save("4");
-    	println(classB.get());
+    	
     }
 }
