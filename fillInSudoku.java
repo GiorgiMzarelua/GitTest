b0e3185cbd574403eca958404f4b0c128f8dda9f
S <s>
2024-01-25 01:15:12 +0400

    {"types":"C","runs":114}

--- fillInSudoku.java
+++ fillInSudoku.java
@@ -5,18 +5,17 @@ import acm.program.ConsoleProgram;
 public class fillInSudoku extends ConsoleProgram{
     public void run(){
     	int[][] sudoku = new int[3][3];
-    	int[]sudoku1 = new int[9];
  //   	int[][] filledSudoku = fillSudoku(sudoku);
-    	sudoku1[0] = 1;
-    	sudoku1[1] = 2;
-    	sudoku1[2] = 3;
-    	sudoku1[3] = 4;
-    	sudoku1[4] = 5;
-    	sudoku1[5] = 6;
-    	sudoku1[6] = 7;
-    	sudoku1[7] = 8;
-    	sudoku1[8] = 9;
-    	println(Arrays.toString(sudoku1));
+    	sudoku[0][0] = 1;
+    	sudoku[0][1] = 2;
+    	sudoku[0][2] = 3;
+    	sudoku[1][0] = 4;
+    	sudoku[1][1] = 5;
+    	sudoku[1][2] = 6;
+    	sudoku[2][0] = 7;
+    	sudoku[2][1] = 8;
+    	sudoku[2][2] = 9;
+    	println(Arrays.toString(sudoku));
     }
 
 	private int[][] fillSudoku(int[][] sudoku) {
