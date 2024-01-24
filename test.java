00403df93fd706ff836e0ae688980c6f1991a424
S <s>
2024-01-24 15:12:32 +0400

    {"types":"C","runs":97}

--- test.java
+++ test.java
@@ -2,9 +2,12 @@ import acm.program.ConsoleProgram;
 
 public class test extends ConsoleProgram{
     public void run(){
-    	String s = "abcd";
-    	String w = s.substring(1,2);
-    	println(w);
+    	for(int i = 2; i < 4201; i++){
+    		if(4201 % i == 0){
+    			println(i);
+    			break;
+    		}
+    	}
     }
 }
 
