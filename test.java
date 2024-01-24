a23a5b41c49b27807ad3c1f5ed344b1483c20b2a
S <s>
2024-01-24 15:13:12 +0400

    {"types":"C","runs":99}

--- test.java
+++ test.java
@@ -2,7 +2,7 @@ import acm.program.ConsoleProgram;
 
 public class test extends ConsoleProgram{
     public void run(){
-    	for(int i = 2; i < 4201; i++){
+    	for(int i = 2; i < 8401; i++){
     		if(4201 % i == 0){
     			println(i);
     			break;
