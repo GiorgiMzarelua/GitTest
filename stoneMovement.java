d99db5e166d628cb5b92ec7f5556a313c307bec7
S <s>
2024-01-25 18:09:00 +0400

    {"types":"C","runs":117}

--- stoneMovement.java
+++ stoneMovement.java
@@ -1,4 +1,34 @@
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
 
-public class stoneMovement {
+import acm.graphics.GCanvas;
+
+public class stoneMovement extends GCanvas implements ComponentListener{
+    public void run(){
+    	System.out.println("32");
+    }
+	@Override
+	public void componentHidden(ComponentEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void componentMoved(ComponentEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void componentResized(ComponentEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void componentShown(ComponentEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
