525628e4028697b4c700f6c07a587f4911323a00
S <s>
2024-01-25 18:25:22 +0400

    {"types":"C","runs":123}

--- applyDiscountToPrices.java
+++ applyDiscountToPrices.java
@@ -1,4 +1,29 @@
+import java.util.StringTokenizer;
 
-public class applyDiscountToPrices {
+import acm.program.ConsoleProgram;
 
+public class applyDiscountToPrices extends ConsoleProgram{
+    public void run(){
+    	String sentence = readLine("Enter the text: ");
+    	int discount = readInt("Enter the discount: ");
+    	String ans = discountedSentence(sentence);
+    	println(ans);
+    }
+
+	private String discountedSentence(String sentence) {
+		
+		StringTokenizer tokenizer = new StringTokenizer(sentence);
+		boolean[] array = new boolean[tokenizer.countTokens()];
+		int i = 0;
+		while(tokenizer.hasMoreTokens()){
+			array[i] = isPrice(tokenizer.nextToken());
+			i++;
+		}
+		return null;
+	}
+
+	private boolean isPrice(String nextToken) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
