d2182428ec399fe47c6f0df3fd203c56748c33d0
S <s>
2024-01-26 16:30:35 +0400

    {"types":"C","runs":1}

--- test.java
+++ test.java
@@ -1,10 +1,58 @@
-import acm.program.ConsoleProgram;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
 
-public class test extends ConsoleProgram{
-    public void run(){
-    	int a = 35;
-    	String s = "";
+import javax.swing.JTextField;
+
+import acm.graphics.GCanvas;
+import acm.graphics.GRect;
+import acm.program.ConsoleProgram;
+import acm.program.GraphicsProgram;
+
+public class test extends GraphicsProgram implements ComponentListener{
+	private JTextField textField;
+	public void init(){
+		addComponentListener(this);
+        addMouseListeners(this);
+        textField = new JTextField(15);
+        textField.addActionListener(this);
+		add(textField, SOUTH);
+		createGrids();
+	}
+	
+    private void createGrids() {
+    	int size = Math.min(getWidth(), getHeight());
+		for(int i = 0; i < 8; i++){
+			for(int j = 0; j < 8; j++){
+				GRect rect = new GRect(size / 8, size / 8);
+				double x = j * size / 8;
+				double y = i * size / 8;
+				add(rect, x, y);
+			}
+		}
+	}
+
+	public void run(){
     
     }
+
+	@Override
+	public void componentHidden(ComponentEvent arg0) {
+		
+	}
+
+	@Override
+	public void componentMoved(ComponentEvent arg0) {
+		
+	}
+
+	@Override
+	public void componentResized(ComponentEvent arg0) {
+		
+	}
+
+	@Override
+	public void componentShown(ComponentEvent arg0) {
+		
+	}
 }
 
