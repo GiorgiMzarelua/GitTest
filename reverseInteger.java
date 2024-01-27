90c8b9791f5b9d0abdbb3de679074a14743f0932
S <s>
2024-01-27 16:05:08 +0400

    {"types":"C","runs":20}

--- reverseInteger.java
+++ reverseInteger.java
@@ -1,4 +1,25 @@
+import acm.program.ConsoleProgram;
 
-public class reverseInteger {
+public class reverseInteger extends ConsoleProgram{
+    public void run(){
+    	int x = readInt("Enter the number to reverse: ");
+    	int ans = reversedInt(x);
+    	println(ans);
+    }
 
-}
+	private int reversedInt(int x) {
+		int y = mod(x);
+		int ans = 0;
+		while(y > 0){
+			ans = 10 * ans + y % 10;
+			y = y / 10;
+		}
+		if( x > 0) return ans;
+		else return -ans;
+	}
+
+	private int mod(int x) {
+		if(x >= 0) return x;
+		else return -x;
+	}
+} 
