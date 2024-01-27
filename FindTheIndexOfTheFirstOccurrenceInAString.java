550d113893eb45ea75f506173dfbd26cb8caf6ab
S <s>
2024-01-27 20:01:17 +0400

    {"types":"C","runs":26}

--- FindTheIndexOfTheFirstOccurrenceInAString.java
+++ FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -1,4 +1,25 @@
+import acm.program.ConsoleProgram;
 
-public class FindTheIndexOfTheFirstOccurrenceInAString {
+public class FindTheIndexOfTheFirstOccurrenceInAString extends ConsoleProgram{
+    public void run(){
+    	String haystack = readLine("Enter the haystack: ");
+    	String needle = readLine("Enter the needle: ");
+    	int ans = findTheIndexOfTheFirstOccurrenceInAString(haystack, needle);
+    	println(ans);
+    }
 
+	private int findTheIndexOfTheFirstOccurrenceInAString(String haystack, String needle) {
+		int hS = haystack.length();
+		int nS = needle.length();
+		for(int i = 0; i < hS; i++){
+			if(haystack.charAt(i) == needle.charAt(0)){
+				int counter = 0;
+				for(int j = i; j < i + nS; j++){
+					if(haystack.charAt(j) == needle.charAt(j)) counter++;
+				}
+				if(counter == nS) return i;
+			}
+		}
+		return -1;
+	}
 }
