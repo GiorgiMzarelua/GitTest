79d911c563074c8109d3be2c99afbf6aee8972b4
S <s>
2024-01-29 00:40:08 +0400

    {"types":"C","runs":49}

--- multiplyStrings.java
+++ multiplyStrings.java
@@ -1,4 +1,25 @@
+import java.util.ArrayList;
 
-public class multiplyStrings {
+import acm.program.ConsoleProgram;
 
+public class multiplyStrings extends ConsoleProgram{
+    public void run(){
+    	String num1 = readLine("Enter the first number: ");
+    	String num2 = readLine("Enter the second number: ");
+    	String ans = multiply(num1, num2);
+    	println(ans);
+    }
+
+	private String multiply(String num1, String num2) {
+		ArrayList<Integer> N1 = new ArrayList<Integer>();
+		ArrayList<Integer> N2 = new ArrayList<Integer>();
+		for(int i = 0; i < num1.length(); i++){
+			N1.add((int) num1.charAt(i) - 48);
+		}
+		for(int i = 0; i < num2.length(); i++){
+			N2.add((int) num2.charAt(i) - 48);
+		}
+		
+		return null;
+	}
 }
