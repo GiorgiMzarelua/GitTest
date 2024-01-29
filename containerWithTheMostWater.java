51bcf580c187c89d1dc4bfb07f91b5ee5d59ae68
S <s>
2024-01-29 20:39:41 +0400

    {"types":"C","runs":1}

--- containerWithTheMostWater.java
+++ containerWithTheMostWater.java
@@ -1,4 +1,30 @@
+import acm.program.ConsoleProgram;
 
-public class containerWithTheMostWater {
+public class containerWithTheMostWater extends ConsoleProgram{
+    public void run(){
+    	int n = readInt("Enter the length of array: ");
+    	int[] height = new int[n];
+    	for(int i = 0; i < n; i++){
+    		height[i] = readInt("Enter the height of the wall: ");
+    	}
+        int ans = maxWater(height);
+        println(ans);
+    }
 
+	private int maxWater(int[] height) {
+		int max = 0;
+	    int i = 0;
+	    int j = height.length - 1;
+	    while(i != j){
+	        int area = (j - i) * (Math.min(height[i],height[j]));
+	        max = Math.max(max,area);
+	        if(height[i] < height[j]){
+	            i++;
+	        }
+	        else{
+	            j--;
+	        }
+	    }
+	    return max;
+    }
 }
