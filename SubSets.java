e47e10acc4e007aeeeedc3090c1ba8bd506dca7e
S <s>
2024-01-30 20:28:53 +0400

    {"types":"C","runs":12}

--- SubSets.java
+++ SubSets.java
@@ -1,4 +1,30 @@
+import java.util.ArrayList;
 
-public class SubSets {
+import acm.program.ConsoleProgram;
 
+public class SubSets extends ConsoleProgram{
+    public void run(){
+    	int n = readInt("Enter the quantity of numbers: ");
+    	int[] array = new int[n];
+    	for(int i = 0; i < n; i++){
+    		array[i] = readInt("Enter the " + (i + 1) + "-th number of the array: ");
+    	}
+    	ArrayList<ArrayList<Integer>> list = allPossibleSubsets(array);
+    	println(list);
+    	
+    }
+
+	private ArrayList<ArrayList<Integer>> allPossibleSubsets(int[] nums) {
+		ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>();
+		list.add(new ArrayList<Integer>());
+		for(int num : nums){
+			int length = list.size();
+			for(int i = 0; i < length; i++){
+				ArrayList<Integer> newList = new ArrayList(list.get(i));
+				newList.add(num);
+				list.add(newList);
+			}
+		}
+		return list;
+	}
 }
