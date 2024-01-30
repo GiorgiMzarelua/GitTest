b45e4ba4b9de602d602426ad043e07924a3fa75c
S <s>
2024-01-30 23:59:58 +0400

    {"types":"C","runs":13}

--- MySet.java
+++ MySet.java
@@ -1,4 +1,49 @@
+import java.util.ArrayList;
+import java.util.Iterator;
 
-public class MySet {
+public class MySet implements MySetInterface{
+	private ArrayList<Object> list;
+    public MySet(){
+    	list = new ArrayList<>();
+    }
 
+	@Override
+	public int size() {
+		return list.size();
+	}
+
+	@Override
+	public boolean isEmpty() {
+		if(list.size() > 0) return true;
+		return false;
+	}
+
+	@Override
+	public boolean contains(Object o) {
+		if(list.contains(o)) return true;
+		return false;
+	}
+
+	@Override
+	public Iterator<Object> iterator() {
+		return list.iterator();
+	}
+
+	@Override
+	public boolean add(Object o) {
+		if(!list.contains(o)){
+			list.add(o);
+			return true;
+		}
+		 return false;
+	}
+
+	@Override
+	public boolean remove(Object o) {
+		if(list.contains(o)){
+			list.remove(o);
+			return true;
+		}
+		return false;
+	}
 }
--- MySetInterface.java
+++ MySetInterface.java
@@ -1,4 +1,10 @@
+import java.util.Iterator;
 
 public interface MySetInterface {
-
+    public int size();
+    public boolean isEmpty();
+    public boolean contains(Object o);
+    public Iterator<Object> iterator();
+    public boolean add(Object o);
+    public boolean remove(Object o);
 }
