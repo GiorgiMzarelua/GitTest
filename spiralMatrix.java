3f5cbc250644255f1aae7883cbdb7ca088792faf
S <s>
2024-01-30 13:04:01 +0400

    {"types":"C","runs":2}

--- spiralMatrix.java
+++ spiralMatrix.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import acm.program.ConsoleProgram;
@@ -12,21 +13,38 @@ public class spiralMatrix extends ConsoleProgram{
     			matrix[i][j] = readInt("Enter the " + (j + 1) + "-th element of " + (i + 1) + "-th array: ");
     		}
     	}
-    	int[] spiral = spiralOrder(matrix);
-    	println(Arrays.toString(spiral));
+    	ArrayList<Integer> spiral = spiralOrder(matrix);
+    	println(spiral);
     }
 
-	private int[] spiralOrder(int[][] matrix) {
-		int[] spiral = new int[matrix.length * matrix[0].length];
-		int k = 0;
-		for(int i = 0; i < matrix.length; i++){
-			for(int j = 0; j < matrix[0].length; j++){
-				if(i <= (matrix.length + 1) / 2 && j != matrix[0].length - 1){
-					spiral[k] = matrix[i][j];
-					k++;
+	private ArrayList<Integer> spiralOrder(int[][] matrix) {
+		ArrayList<Integer> spiral = new ArrayList<Integer>();
+		int row = 0;
+		int col = 0;
+		int rows = matrix.length - 1;
+		int colls = matrix[0].length - 1;
+		while(row <= rows && col <= colls){
+			for(int i = col; i <= colls; i++){
+				spiral.add(matrix[row][i]);
+			}
+			row++;
+			for(int i = row; i <= rows; i++){
+				spiral.add(matrix[i][col]);
+			}
+			colls--;
+			if(rows >= row){
+				for(int i = colls; i >= col; i--){
+					spiral.add(matrix[rows][i]);
 				}
 			}
+			rows--;
+			if(colls >= col){
+				for(int i = rows; i >= row; i--){
+					spiral.add(matrix[i][colls]);
+				}
+			}
+			col++;
 		}
-		return null;
+		return spiral;
 	}
 }
