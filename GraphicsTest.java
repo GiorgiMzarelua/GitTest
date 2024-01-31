b6e110ec5f7001503fae0d665cc0735ea1cf8438
S <s>
2024-01-31 20:42:46 +0400

    {"types":"C","runs":39}

--- GraphicsTest.java
+++ GraphicsTest.java
@@ -1,4 +1,24 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ComponentListener;
 
-public class GraphicsTest {
+import javax.swing.JButton;
+import javax.swing.JWindow;
 
+import acm.program.ConsoleProgram;
+
+public class GraphicsTest extends ConsoleProgram{
+	private JButton open;
+	private JButton close;
+	private JWindow window;
+    public void init(){
+    	open = new JButton("open");
+    	open.addActionListener(this);
+    	add(open, SOUTH);
+    }
+    
+    public void ActionPerformed(ActionEvent e){
+    	if(e.getActionCommand().equals("open")){
+    		println(12);
+    	}
+    }
 }
