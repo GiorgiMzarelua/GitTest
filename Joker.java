1ad0084e75840bd95422973b39a2c0118dc63789
S <s>
2024-01-31 18:14:38 +0400

    {"types":"C","runs":28}

--- Joker.java
+++ Joker.java
@@ -1,6 +1,8 @@
 import acm.program.ConsoleProgram;
+import acm.util.RandomGenerator;
 
 public class Joker extends ConsoleProgram{
+	RandomGenerator rgen = RandomGenerator.getInstance();
     public void run(){
     	int n = readInt("Enter the quantity of experiments: ");
     	double avg = simulateJoker(n);
@@ -15,8 +17,22 @@ public class Joker extends ConsoleProgram{
 		return counter / n;
 	}
 
-	private double simulateOne() {
-		
-		return 0;
+	private int simulateOne() {
+		int tries = 0;
+		while(true){
+			tries++;
+			int[] joker = new int[36];
+			for(int i = 0; i < 36; i++){
+				int num = rgen.nextInt(0, 35);
+				if(joker[num] == 0 && i % 4 == 0) joker[num] = 1;
+				else if(joker[num] == 0 && i % 2 == 1 || i % 2 == 2 || i % 2 == 3) joker[num] = -1;
+				else i--;
+			}
+			int countJokers = 0;
+			for(int i = 0; i < 2; i++){
+				if(joker[i] == 1) countJokers++;
+				if(countJokers == 2) return tries;
+			}
+		}
 	}
 }
