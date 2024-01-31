ebd6134dd55557ee735c61a1c9755873b6ff82ed
S <s>
2024-01-31 17:13:51 +0400

    {"types":"C","runs":10}

--- Domino.java
+++ Domino.java
@@ -1,4 +1,21 @@
+import acm.program.ConsoleProgram;
+import acm.util.RandomGenerator;
 
-public class Domino {
+public class Domino extends ConsoleProgram{
+	RandomGenerator rgen = RandomGenerator.getInstance();
+    public void run(){
+    	int n = readInt("Enter the number of experiments: ");
+    	double avg = simulateDomino(n);
+        println(avg);
+    }
 
+	private double simulateDomino(int n) {
+		while(n > 0){
+			int num1 = rgen.nextInt(0, 6);
+			int num2 = rgen.nextInt(0, 6);
+			println(num1 + " " + num2);
+			return;
+		}
+		return 0;
+	}
 }
