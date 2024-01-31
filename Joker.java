6c63752838aa1e0b7824a8c14826cb36bd786223
S <s>
2024-01-31 18:00:28 +0400

    {"types":"C","runs":27}

--- Joker.java
+++ Joker.java
@@ -1,4 +1,22 @@
+import acm.program.ConsoleProgram;
 
-public class Joker {
+public class Joker extends ConsoleProgram{
+    public void run(){
+    	int n = readInt("Enter the quantity of experiments: ");
+    	double avg = simulateJoker(n);
+    	println(avg);
+    }
 
+	private double simulateJoker(int n) {
+		double counter = 0;
+		for(int i = 0; i < n; i++){
+			counter += simulateOne();
+		}
+		return counter / n;
+	}
+
+	private double simulateOne() {
+		
+		return 0;
+	}
 }
