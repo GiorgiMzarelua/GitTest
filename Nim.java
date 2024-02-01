15bb2c2c52da097afe6c2f2712960700cbada420
S <s>
2024-02-01 11:59:00 +0400

    {"types":"C","runs":55}

--- Nim.java
+++ Nim.java
@@ -1,4 +1,24 @@
+import java.awt.Color;
 
-public class Nim {
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
 
+public class Nim extends GraphicsProgram{
+	public static final int N_COINS = 11; 
+	public static final int COIN_SIZE = 25; 
+	public static final int COIN_SEP = 10;
+    public void run(){
+    	addBalls();
+    }
+
+	private void addBalls() {
+		for(int i = 0; i < N_COINS; i++){
+			GOval ball = new GOval (COIN_SIZE, COIN_SIZE);
+			double x = i * COIN_SIZE + i * COIN_SEP;
+			double y = getHeight() / 2 - COIN_SIZE / 2;
+			ball.setFilled(true);
+			ball.setColor(Color.GRAY);
+			add(ball, x, y);
+		}
+	}
 }
