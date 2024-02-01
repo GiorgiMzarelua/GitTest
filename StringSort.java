4a0eb9d32a4b377de26f9e56df2f2ec5e5876b60
S <s>
2024-02-01 16:23:00 +0400

    {"types":"C","runs":60}

--- StringSort.java
+++ StringSort.java
@@ -7,7 +7,7 @@ public class StringSort extends ConsoleProgram{
 	  String[] stringArray = new String[3];
 	  stringArray[0] = "aba";
 	  stringArray[1] = "abdc";
-	  stringArray[2] = "fdsfd";
+	  stringArray[2] = "aa";
 	  Arrays.sort(stringArray);
 	  println(Arrays.toString(stringArray));
   }
